92abebc3d4af42497193a5e42f31a31b57c98de6
nsimpson <nsimpson@df51345c-241b-11df-8743-957545a88a67>
2008-01-17 01:35:50 +0000

    PDF Viewer app

---
+++ trunk/src/modules/apps/2d/pdfviewer/src/classes/org/jdesktop/lg3d/wonderland/pdfviewer/common/PDFCellMessage.java
@@ -0,0 +1,176 @@
+/**
+ * Project Looking Glass
+ *
+ * $RCSfile$
+ *
+ * Copyright (c) 2004-2007, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.lg3d.wonderland.pdfviewer.common;
+
+import java.awt.Point;
+import java.nio.ByteBuffer;
+import java.util.logging.Logger;
+import org.jdesktop.lg3d.wonderland.darkstar.common.CellID;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.CellMessage;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataDouble;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataInt;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataString;
+
+/**
+ * A Cell Message that carries PDF Viewer actions
+ *
+ * @author nsimpson
+ */
+public class PDFCellMessage extends CellMessage {
+    
+    private static final Logger logger =
+            Logger.getLogger(PDFCellMessage.class.getName());
+    
+    public enum Action { OPEN_DOCUMENT, CLOSE_DOCUMENT, SHOW_PAGE, SET_VIEW_POSITION };
+    
+    private Action action;
+    private String doc;
+    private int page;
+    private Point position;
+    
+    public PDFCellMessage() {
+        super();
+    }
+    
+    public PDFCellMessage(Action action) {
+        this(action, null, 0, null);
+    }
+
+    
+    public PDFCellMessage(Action action, String doc, int page, Point position) {
+        super();
+        this.action = action;
+        this.doc = doc;
+        this.page = page;
+        this.position = position;
+    }
+    
+    public PDFCellMessage(CellID cellID, Action action) {
+        this(cellID, action, null, 0, null);
+    }
+    
+    public PDFCellMessage(CellID cellID, Action action, String doc, int page, Point position) {
+        super(cellID);
+        this.action = action;
+        this.doc = doc;
+        this.page = page;
+        this.position = position;
+    }
+    
+    /**
+     * Set the action
+     * @param action the action
+     */
+    public void setAction(Action action) {
+        this.action = action;
+    }
+    
+    /**
+     * Get the action
+     * @return the action
+     */
+    public Action getAction() {
+        return action;
+    }
+    
+    /** 
+     * Set the URL of the document
+     * @param doc the URL of the document
+     */
+    public void setDocument(String doc) {
+        this.doc = doc;
+    }
+    
+    /**
+     * Get the document URL
+     * @return the URL of the document
+     */
+    public String getDocument() {
+        return doc;
+    }
+    
+    /**
+     * Set the currently selected page
+     * @param page the page to go to
+     */
+    public void setPage(int page) {
+        this.page = page;
+    }
+    
+    /** 
+     * Get the currently selected page
+     * @return the current page number
+     */
+    public int getPage() {
+        return page;
+    }
+    
+    /**
+     * Set the (x, y) position of the page
+     * @param position the (x, y) position of the page
+     */
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+    
+    /**
+     * Get the (x, y) position of the page
+     * @return the (x, y) position
+     */
+    public Point getPosition() {
+        return position;
+    }
+    
+    /** 
+     * Get a string representation of the PDF cell message
+     * @return a the cell message as as String
+     */
+    public String toString() {
+        return getAction() + ", " + getDocument() + ", " + getPage() + ", " + getPosition();
+    }
+    
+    /**
+     * Extract the message from binary data
+     */
+    @Override
+    protected void extractMessageImpl(ByteBuffer data) {
+        super.extractMessageImpl(data);
+        
+        action = Action.values()[DataInt.value(data)];
+        doc = DataString.value(data);
+        page = DataInt.value(data);
+        position = new Point((int)DataDouble.value(data), (int)DataDouble.value(data));
+       
+    }
+    
+    /**
+     * Create a binry version of the message
+     */
+    @Override
+    protected void populateDataElements() {
+        super.populateDataElements();
+        
+        dataElements.add(new DataInt(action.ordinal()));
+        dataElements.add(new DataString(doc));        
+        dataElements.add(new DataInt(page));
+        dataElements.add(new DataDouble(position.getX()));
+        dataElements.add(new DataDouble(position.getY()));        
+    }
+}
---
+++ trunk/src/modules/apps/2d/pdfviewer/src/classes/org/jdesktop/lg3d/wonderland/pdfviewer/common/PDFViewerCellSetup.java
@@ -0,0 +1,113 @@
+/**
+ * Project Looking Glass
+ *
+ * $RCSfile$
+ *
+ * Copyright (c) 2004-2007, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.lg3d.wonderland.pdfviewer.common;
+
+import java.awt.Point;
+import java.util.logging.Logger;
+import javax.vecmath.Matrix4f;
+import org.jdesktop.lg3d.wonderland.darkstar.common.setup.SharedApp2DCellSetup;
+
+/**
+ * Container for PDF Viewer cell data
+ *
+ * @author nsimpson
+ */
+public class PDFViewerCellSetup extends SharedApp2DCellSetup {
+    
+    private static final Logger logger =
+            Logger.getLogger(PDFViewerCellSetup.class.getName());
+    
+    private String document;
+    private int page = 1;
+    private Point position = new Point(0 ,0);
+    private String checksum;
+    
+    public PDFViewerCellSetup() {
+        this(null, null);
+    }
+    
+    public PDFViewerCellSetup(String appName, Matrix4f viewRectMat) {
+        super(appName, viewRectMat);
+    }
+    
+    /*
+     * Set the URL of the PDF document associated with this cell
+     * @param doc the URL of the PDF document
+     */
+    public void setDocument(String document) {
+        this.document = document;
+    }
+    
+    /*
+     * Get the URL of the PDF document associated with this cell
+     * @return the URL of the PDF document
+     */
+    public String getDocument() {
+        return document;
+    }
+    
+    /*
+     * Set the current page
+     * @param page the current page in the PDF document
+     */
+    public void setPage(int page) {
+        this.page = page;
+    }
+    
+    /*
+     * Get the current page
+     * @return the current page
+     */
+    public int getPage() {
+        return page;
+    }
+    
+    /*
+     * Set the page position
+     * @param position the scroll position in x and y coordinates
+     */
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+    
+    /*
+     * Get the page position
+     * @return the scroll position of the page
+     */
+    public Point getPosition() {
+        return position;
+    }
+    
+    /*
+     * Get the checksum for the PDF document
+     * @return the checksm of the PDF document
+     */
+    public String getChecksum() {
+        return checksum;
+    }
+    
+    /*
+     * Set the checksum of the PDF document
+     * @param checksum the checksum of the PDF document
+     */
+    public void setChecksum(String checksum) {
+        this.checksum = checksum;
+    }
+}
