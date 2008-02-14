da4587e8f32928ff1d2dd96871439df70d21fce3
jprovino <jprovino@df51345c-241b-11df-8743-957545a88a67>
2008-02-14 21:47:18 +0000

    Changes for volume control.

--- trunk/src/modules/apps/3d/audiorecorder/src/classes/org/jdesktop/lg3d/wonderland/audiorecorder/common/AudioRecorderCellMessage.java
+++ trunk/src/modules/apps/3d/audiorecorder/src/classes/org/jdesktop/lg3d/wonderland/audiorecorder/common/AudioRecorderCellMessage.java
@@ -23,27 +23,54 @@ import java.nio.ByteBuffer;
 import org.jdesktop.lg3d.wonderland.darkstar.common.CellID;
 import org.jdesktop.lg3d.wonderland.darkstar.common.messages.CellMessage;
 import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataBoolean;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataDouble;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataInt;
 import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataString;
+
+import org.jdesktop.lg3d.wonderland.audiorecorder.common.AudioRecorderMessage.RecorderAction;
 
 /**
  *
  */
 public class AudioRecorderCellMessage extends CellMessage {
+    private RecorderAction action;
+
     private boolean isRecording;
     private boolean isPlaying;
     private String userName;
     
+    private double volume;
+
     public AudioRecorderCellMessage() {
         super();
     }
     
     public AudioRecorderCellMessage(CellID cellID, boolean isRecording, boolean isPlaying, String userName) {
-        super(cellID);
+
+	super(cellID);
+
+	this.action = RecorderAction.SETUP_RECORDER;
         this.isRecording = isRecording;
         this.isPlaying = isPlaying;
         this.userName = userName;
     }
    
+    public AudioRecorderCellMessage(CellID cellID, boolean isRecording, boolean isPlaying, String userName,
+	    double volume) {
+
+	super(cellID);
+
+	this.action = RecorderAction.SET_VOLUME;
+        this.isRecording = isRecording;
+        this.isPlaying = isPlaying;
+        this.userName = userName;
+	this.volume = volume;
+    }
+
+    public RecorderAction getAction() {
+	return action;
+    }
+
     public boolean isRecording() {
         return isRecording;
     }
@@ -52,23 +79,41 @@ public class AudioRecorderCellMessage extends CellMessage {
         return isPlaying;
     }
 
+    public double getVolume() {
+	return volume;
+    }
+
     @Override
     protected void extractMessageImpl(ByteBuffer data) {
-        super.extractMessageImpl(data);
+	super.extractMessageImpl(data);
+
+	action = RecorderAction.values()[DataInt.value(data)];
+
         isRecording = DataBoolean.value(data);
         isPlaying = DataBoolean.value(data);
         userName = DataString.value(data);
+
+	if (action.equals(RecorderAction.SET_VOLUME)) {
+	    volume = DataDouble.value(data);
+	}
     }
 
     @Override
     protected void populateDataElements() {
-        super.populateDataElements();
+	super.populateDataElements();
+
+	dataElements.add(new DataInt(action.ordinal()));
         dataElements.add(new DataBoolean(isRecording));
         dataElements.add(new DataBoolean(isPlaying));
         dataElements.add(new DataString(userName));
+
+	if (action.equals(RecorderAction.SET_VOLUME)) {
+	    dataElements.add(new DataDouble(volume));
+	}
     }
 
     public String getUserName() {
         return userName;
     }
+
 }
