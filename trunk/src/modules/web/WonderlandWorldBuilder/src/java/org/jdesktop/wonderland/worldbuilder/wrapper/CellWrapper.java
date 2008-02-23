1ca232654a42d438e41d8eefdc7b7002dd649b6c
kaplanj <kaplanj@df51345c-241b-11df-8743-957545a88a67>
2008-02-23 23:13:08 +0000

    Initial code for World Builder  - server side components only

--- trunk/src/modules/web/WonderlandWorldBuilder/src/java/org/jdesktop/wonderland/worldbuilder/wrapper/CellWrapper.java
+++ trunk/src/modules/web/WonderlandWorldBuilder/src/java/org/jdesktop/wonderland/worldbuilder/wrapper/CellWrapper.java
@@ -1,15 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Project Looking Glass
+ *
+ * $RCSfile$
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
  */
-
 package org.jdesktop.wonderland.worldbuilder.wrapper;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
