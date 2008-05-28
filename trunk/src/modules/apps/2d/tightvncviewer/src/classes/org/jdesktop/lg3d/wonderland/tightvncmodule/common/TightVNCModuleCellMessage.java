b561f007f34734e04170fce7e55072c54e2c92f5
nsimpson <nsimpson@df51345c-241b-11df-8743-957545a88a67>
2008-05-28 18:44:22 +0000

    Add HUD controls: state sync/unsync, ability to open a new session, and close a session.

--- trunk/src/modules/apps/2d/tightvncviewer/src/classes/org/jdesktop/lg3d/wonderland/tightvncmodule/common/TightVNCModuleCellMessage.java
+++ trunk/src/modules/apps/2d/tightvncviewer/src/classes/org/jdesktop/lg3d/wonderland/tightvncmodule/common/TightVNCModuleCellMessage.java
@@ -20,24 +20,200 @@
 package org.jdesktop.lg3d.wonderland.tightvncmodule.common;
 
 import java.nio.ByteBuffer;
+import org.jdesktop.lg3d.wonderland.darkstar.common.CellID;
 import org.jdesktop.lg3d.wonderland.darkstar.common.messages.CellMessage;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataInt;
+import org.jdesktop.lg3d.wonderland.darkstar.common.messages.DataString;
 
 /**
  *
  * @author nsimpson
  */
 public class TightVNCModuleCellMessage extends CellMessage {
+
+    private static final int DEFAULT_VNC_PORT = 5900;
+
+    public enum Action {
+
+        UNKNOWN,
+        REQUEST_DENIED, REQUEST_COMPLETE,
+        OPEN_SESSION,
+        CLOSE_SESSION,
+        GET_STATE, SET_STATE
+    };
+    private String uid;
+    private Action action = Action.UNKNOWN;
+    private String vncServer;
+    private int vncPort = DEFAULT_VNC_PORT;
+    private String vncUsername;
+    private String vncPassword;
+
     public TightVNCModuleCellMessage() {
         super();
     }
-    
+
+    public TightVNCModuleCellMessage(Action action) {
+        this(action, null, 0, null, null);
+    }
+
+    public TightVNCModuleCellMessage(Action action, String vncServer, int vncPort, String vncUsername, String vncPassword) {
+        super();
+        this.action = action;
+        this.vncServer = vncServer;
+        this.vncPort = vncPort;
+        this.vncUsername = vncUsername;
+        this.vncPassword = vncPassword;
+    }
+
+    public TightVNCModuleCellMessage(CellID cellID, Action action) {
+        super(cellID);
+        setAction(action);
+    }
+
+    public TightVNCModuleCellMessage(CellID cellID, String uid, Action action, String vncServer, int vncPort, String vncUsername, String vncPassword) {
+        super(cellID);
+        setUID(uid);
+        this.action = action;
+        this.vncServer = vncServer;
+        this.vncPort = vncPort;
+        this.vncUsername = vncUsername;
+        this.vncPassword = vncPassword;
+    }
+
+    public TightVNCModuleCellMessage(TightVNCModuleCellMessage msg) {
+        this.setAction(msg.getAction());
+        this.setUID(msg.getUID());
+        setCellID(msg.getCellID());
+        this.setServer(msg.getServer());
+        this.setPort(msg.getPort());
+        this.setUsername(msg.getUsername());
+        this.setPassword(msg.getPassword());
+    }
+
+    public void setUID(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUID() {
+        return uid;
+    }
+
+    /**
+     * Set the action
+     * @param action the action
+     */
+    public void setAction(Action action) {
+        this.action = action;
+    }
+
+    /**
+     * Get the action
+     * @return the action
+     */
+    public Action getAction() {
+        return action;
+    }
+
+    /**
+     * Set the hostname of the VNC server
+     * @param vncServer the hostname of the VNC server
+     */
+    public void setServer(String vncServer) {
+        this.vncServer = vncServer;
+    }
+
+    /**
+     * Get the hostname of the VNC server
+     * @return the hostname of the VNC server
+     */
+    public String getServer() {
+        return vncServer;
+    }
+
+    /**
+     * Set the VNC server's port number 
+     * @param vncPort the VNC server's port number
+     */
+    public void setPort(int vncPort) {
+        this.vncPort = vncPort;
+    }
+
+    /**
+     * Get the VNC server's port numer
+     * @return the port number of the VNC server
+     */
+    public int getPort() {
+        return vncPort;
+    }
+
+    /**
+     * Set the VNC username 
+     * @param vncUsername the name of the user establishing the VNC session
+     */
+    public void setUsername(String vncUsername) {
+        this.vncUsername = vncUsername;
+    }
+
+    /**
+     * Get the VNC username
+     * @return the name of the user for establishing the VNC session
+     */
+    public String getUsername() {
+        return vncUsername;
+    }
+
+    /**
+     * Set the VNC password 
+     * @param vncPassword the password of the user establishing the VNC session
+     */
+    public void setPassword(String vncPassword) {
+        this.vncPassword = vncPassword;
+    }
+
+    /**
+     * Get the VNC password
+     * @return the password of the user establishing the VNC session
+     */
+    public String getPassword() {
+        return vncPassword;
+    }
+
+    /** 
+     * Get a string representation of the VNC cell message
+     * @return a the cell message as as String
+     */
+    @Override
+    public String toString() {
+        return getAction() + ", " + getServer() + ", " + getPort() + ", " + getUsername();
+    }
+
+    /**
+     * Extract the message from binary data
+     */
     @Override
     protected void extractMessageImpl(ByteBuffer data) {
         super.extractMessageImpl(data);
+
+        uid = DataString.value(data);
+        action = Action.values()[DataInt.value(data)];
+        vncServer = DataString.value(data);
+        vncPort = DataInt.value(data);
+        vncUsername = DataString.value(data);
+        vncPassword = DataString.value(data);
     }
-    
+
+    /**
+     * Create a binary version of the message
+     */
     @Override
     protected void populateDataElements() {
         super.populateDataElements();
+
+        dataElements.add(new DataString(uid));
+        dataElements.add(new DataInt(action.ordinal()));
+        dataElements.add(new DataString(vncServer));
+        dataElements.add(new DataInt(vncPort));
+        dataElements.add(new DataString(vncUsername));
+        dataElements.add(new DataString(vncPassword));
     }
 }
