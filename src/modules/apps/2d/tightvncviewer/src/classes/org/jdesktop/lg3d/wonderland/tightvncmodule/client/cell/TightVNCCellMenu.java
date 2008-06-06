2c2c0605a5ca37b5cfa3ff51ed9e936643bc6910
nsimpson <nsimpson@df51345c-241b-11df-8743-957545a88a67>
2008-06-06 23:05:54 +0000

    debug message for morrisford

--- src/modules/apps/2d/tightvncviewer/src/classes/org/jdesktop/lg3d/wonderland/tightvncmodule/client/cell/TightVNCCellMenu.java
+++ src/modules/apps/2d/tightvncviewer/src/classes/org/jdesktop/lg3d/wonderland/tightvncmodule/client/cell/TightVNCCellMenu.java
@@ -74,6 +74,7 @@ public class TightVNCCellMenu extends CellMenu {
         // calculate the maximum width of each button position in the
         // HUD menu
         for (Button b : Button.values()) {
+            System.err.println("--- vnc loading HUD button: " + b.getIconImageName());
             position = b.getPosition();
 
             URL buttonURL = TightVNCCellMenu.class.getResource(b.getIconImageName());
