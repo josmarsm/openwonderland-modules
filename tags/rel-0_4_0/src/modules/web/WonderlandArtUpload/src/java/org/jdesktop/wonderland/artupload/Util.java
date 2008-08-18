e311cd1d8ab8675731320530261c02df890dd36c
kaplanj <kaplanj@df51345c-241b-11df-8743-957545a88a67>
2008-08-18 23:10:41 +0000

    Tagged workspace for 0.4 release

---
+++ tags/rel-0_4_0/src/modules/web/WonderlandArtUpload/src/java/org/jdesktop/wonderland/artupload/Util.java
@@ -0,0 +1,105 @@
+/**
+ * Project Looking Glass
+ *
+ * $RCSfile:$
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision:$
+ * $Date:$
+ * $State:$
+ */
+
+package org.jdesktop.wonderland.artupload;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import org.jdesktop.lg3d.wonderland.servlet.WonderlandServletUtil;
+import org.jdesktop.lg3d.wonderland.wfs.WFSCellDirectory;
+
+/**
+ *
+ * @author jkaplan
+ */
+public class Util {
+    public static final String ART_DIR_PROP   = "wonderland.art.url.local";
+    public static final String ART_URL_PROP   = "wonderland.art.url.base";
+    public static final String ART_REDIR_PROP = "wonderland.art.url.redirect";
+    public static final String WFS_ROOT_PROP  = "wonderland.wfs.root";
+    public static final String WFS_DIR_PROP   = "wonderland.wfs.upload.dir";
+    
+    private static final String BASE_DIR = File.separator + ".wonderland" + 
+                                           File.separator + "artUpload";
+    private static final String ART_DIR = BASE_DIR + File.separator + "art";
+    private static final String WFS_DIR = BASE_DIR + File.separator + "upload-wfs";
+
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
+    
+    /**
+     * Get the art directory
+     */
+    public static File getArtDir(ServletContext context) throws IOException {
+        String defaultArtDir = "file:" + System.getProperty("user.home") +
+                               ART_DIR;
+        
+        String artDir = WonderlandServletUtil.getProperty(ART_DIR_PROP, context, 
+                                                          defaultArtDir);
+        
+        return new File(new URL(artDir).getPath());
+    }
+    
+    /**
+     * Determine whether art URL is local or remote
+     */
+    public static boolean isLocalRedirect(ServletContext context) {
+        return (getArtRedirectURL(context) != null);
+    }
+    
+    /**
+     * Get the remote art URL
+     */
+    public static String getArtURL(ServletContext context) {
+        return WonderlandServletUtil.getProperty(ART_URL_PROP, context);
+    }
+    
+    /**
+     * Get the local art URL
+     */
+    public static String getArtRedirectURL(ServletContext context) {
+        return WonderlandServletUtil.getProperty(ART_REDIR_PROP, context);
+    }
+    
+    /**
+     * Get the WFS directory
+     */
+    public static WFSCellDirectory getWFS(ServletContext context) 
+            throws IOException 
+    {
+        // get the wfs root
+        String wfsRootDefault = "file:" + System.getProperty("user.home") + 
+                                WFS_DIR; 
+        String wfsRoot = WonderlandServletUtil.getProperty(WFS_ROOT_PROP, 
+                                context, wfsRootDefault);
+        URL wfsRootURL = new URL(wfsRoot);
+        
+        // get the subdirectory, if any
+        String wfsDir = WonderlandServletUtil.getProperty(WFS_DIR_PROP, 
+                                context);
+        
+        // open or create the directory
+        return WonderlandServletUtil.openWFS(wfsRootURL, wfsDir, true);
+    }
+}
