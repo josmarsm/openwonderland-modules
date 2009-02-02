77b7133f1dca428a5b5941821e56f4d281a3f0f1
bernard_horan <bernard_horan@df51345c-241b-11df-8743-957545a88a67>
2009-02-02 11:18:10 +0000

    Fix licence headers

--- trunk/0.5/unstable/audiorecorder-module/src/classes/org/jdesktop/wonderland/modules/audiorecorder/common/AudioRecorderCellServerState.java
+++ trunk/0.5/unstable/audiorecorder-module/src/classes/org/jdesktop/wonderland/modules/audiorecorder/common/AudioRecorderCellServerState.java
@@ -1,13 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
  */
 
 package org.jdesktop.wonderland.modules.audiorecorder.common;
 
 import java.io.CharArrayReader;
 import java.io.CharArrayWriter;
-import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
