1e09c58019f7d2a9c1974f861f920102e193d533
morrisford <morrisford@df51345c-241b-11df-8743-957545a88a67>
2009-06-02 21:55:11 +0000

    Redo commit from this morning.

--- tags/0.5-dev5/unstable/scriptingComponent/src/classes/org/jdesktop/wonderland/modules/scriptingComponent/server/ScriptingComponentMO.java
+++ tags/0.5-dev5/unstable/scriptingComponent/src/classes/org/jdesktop/wonderland/modules/scriptingComponent/server/ScriptingComponentMO.java
@@ -50,17 +50,48 @@ public class ScriptingComponentMO extends CellComponentMO
     private ManagedReference<ChannelComponentMO> channelComponentRef;
 
     private ManagedReference<ScriptingComponentChangeReceiver> receiverRef;
-    
+
+    private int totalEvents = 30;
+
     private String info;
     private String cellName;
     private String scriptURL;
-    private String[] eventNames;
-    private String[] eventScriptType;
+//    private String[] eventNames;
+    private String[] eventNames = new String[totalEvents];
+//    private String[] eventScriptType;
+    private String[] eventScriptType = new String[totalEvents];;
     private int      iceCode;
     private String   payload;
     private Vector3f  translateTransform;
     private Quaternion  rotateTransform;
     private Vector3f  scaleTransform;
+
+    public static final int MOUSE1_EVENT = 0;
+    public static final int MOUSE2_EVENT = 1;
+    public static final int MOUSE3_EVENT = 2;
+    public static final int MOUSE1S_EVENT = 3;
+    public static final int MOUSE2S_EVENT = 4;
+    public static final int MOUSE3S_EVENT = 5;
+    public static final int MOUSE1C_EVENT = 6;
+    public static final int MOUSE2C_EVENT = 7;
+    public static final int MOUSE3C_EVENT = 8;
+    public static final int MOUSE1A_EVENT = 9;
+    public static final int MOUSE2A_EVENT = 10;
+    public static final int MOUSE3A_EVENT = 11;
+
+    public static final int TIMER_EVENT = 12;
+    public static final int STARTUP_EVENT = 13;
+    public static final int PROXIMITY_EVENT = 14;
+
+    public static final int MESSAGE1_EVENT = 15;
+    public static final int MESSAGE2_EVENT = 16;
+    public static final int MESSAGE3_EVENT = 17;
+    public static final int MESSAGE4_EVENT = 18;
+
+    public static final int INTERCELL_EVENT = 19;
+    public static final int CHAT_EVENT = 20;
+    public static final int PRESENCE_EVENT = 21;
+
     
      /**
      * Create a ScriptingComponent for the given cell. The cell must already
@@ -68,6 +99,7 @@ public class ScriptingComponentMO extends CellComponentMO
      * @param cell
      */
 
+    
     public ScriptingComponentMO(CellMO cell)
         {
         super(cell);
@@ -75,6 +107,56 @@ public class ScriptingComponentMO extends CellComponentMO
         // set up the reference to the receiver
         ScriptingComponentChangeReceiver receiver = new ScriptingComponentChangeReceiver(cellRef, this);
         receiverRef = AppContext.getDataManager().createReference(receiver);
+
+        info = "Default";
+        cellName = "default";
+        scriptURL = "http://172.16.126.9:8080/webdav/content/";
+        eventNames[MOUSE1_EVENT] = "mouse1.js";
+        eventNames[MOUSE2_EVENT] = "mouse2.js";
+        eventNames[MOUSE3_EVENT] = "mouse3.js";
+        eventNames[MOUSE1S_EVENT] = "mouse1s.js";
+        eventNames[MOUSE2S_EVENT] = "mouse2s.js";
+        eventNames[MOUSE3S_EVENT] = "mouse3s.js";
+        eventNames[MOUSE1C_EVENT] = "mouse1c.js";
+        eventNames[MOUSE2C_EVENT] = "mouse2c.js";
+        eventNames[MOUSE3C_EVENT] = "mouse3c.java";
+        eventNames[MOUSE1A_EVENT] = "mouse1a.js";
+        eventNames[MOUSE2A_EVENT] = "mouse2a.js";
+        eventNames[MOUSE3A_EVENT] = "mouse3a.js";
+        eventNames[TIMER_EVENT] = "timer.js";
+        eventNames[STARTUP_EVENT] = "startup.js";
+        eventNames[PROXIMITY_EVENT] = "prox.js";
+        eventNames[MESSAGE1_EVENT] = "message1.js";
+        eventNames[MESSAGE2_EVENT] = "message2.js";
+        eventNames[MESSAGE3_EVENT] = "message3.js";
+        eventNames[MESSAGE4_EVENT] = "message4.js";
+        eventNames[INTERCELL_EVENT] = "ice.js";
+        eventNames[CHAT_EVENT] = "chat.js";
+        eventNames[PRESENCE_EVENT] = "presence.js";
+
+        eventScriptType[MOUSE1_EVENT] = "javascript";
+        eventScriptType[MOUSE2_EVENT] = "javascript";
+        eventScriptType[MOUSE3_EVENT] = "javascript";
+        eventScriptType[MOUSE1S_EVENT] = "javascript";
+        eventScriptType[MOUSE2S_EVENT] = "javascript";
+        eventScriptType[MOUSE3S_EVENT] = "javascript";
+        eventScriptType[MOUSE1C_EVENT] = "javascript";
+        eventScriptType[MOUSE2C_EVENT] = "javascript";
+        eventScriptType[MOUSE3C_EVENT] = "java";
+        eventScriptType[MOUSE1A_EVENT] = "javascript";
+        eventScriptType[MOUSE2A_EVENT] = "javascript";
+        eventScriptType[MOUSE3A_EVENT] = "javascript";
+        eventScriptType[TIMER_EVENT] = "javascript";
+        eventScriptType[STARTUP_EVENT] = "javascript";
+        eventScriptType[PROXIMITY_EVENT] = "javascript";
+        eventScriptType[MESSAGE1_EVENT] = "javascript";
+        eventScriptType[MESSAGE2_EVENT] = "javascript";
+        eventScriptType[MESSAGE3_EVENT] = "javascript";
+        eventScriptType[MESSAGE4_EVENT] = "javascript";
+        eventScriptType[INTERCELL_EVENT] = "javascript";
+        eventScriptType[CHAT_EVENT] = "javascript";
+        eventScriptType[PRESENCE_EVENT] = "javascript";
+
         }
 
     @Override
