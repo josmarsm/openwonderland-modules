733b07b88ded73ee0e815bfa5690c5274a8e8df1
drew_harry <drew_harry@df51345c-241b-11df-8743-957545a88a67>
2009-07-08 18:23:20 +0000

    Lots of changes, including slightly better behaved label nodes (though they still have some troubles), non-uniform scaling working client-side, and some other polish. Other than the proximity bugs (which will be fixed in core), it's mostly rendering bugs left.

--- 0.5/unstable/chat-zones/src/classes/org/jdesktop/wonderland/modules/chatzones/client/ChatZonesCell.java
+++ 0.5/unstable/chat-zones/src/classes/org/jdesktop/wonderland/modules/chatzones/client/ChatZonesCell.java
@@ -20,6 +20,7 @@ package org.jdesktop.wonderland.modules.chatzones.client;
 
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import org.jdesktop.mtgame.RenderUpdater;
 import org.jdesktop.wonderland.client.cell.Cell;
 import org.jdesktop.wonderland.client.cell.Cell.RendererType;
 import org.jdesktop.wonderland.client.cell.CellCache;
@@ -28,6 +29,7 @@ import org.jdesktop.wonderland.client.cell.ChannelComponent;
 import org.jdesktop.wonderland.client.cell.ChannelComponent.ComponentMessageReceiver;
 import org.jdesktop.wonderland.client.input.Event;
 import org.jdesktop.wonderland.client.input.EventClassListener;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
 import org.jdesktop.wonderland.client.jme.input.MouseButtonEvent3D;
 import org.jdesktop.wonderland.common.cell.CellID;
 import org.jdesktop.wonderland.common.cell.CellStatus;
@@ -49,7 +51,11 @@ public class ChatZonesCell extends Cell {
             Logger.getLogger(ChatZonesCell.class.getName());
 
     private int numAvatarsInZone = 0;
-    private String label = "Media Lab";
+
+    // It's a single space because an empty string causes the label node
+    // to barf. TODO fix the label node so it can more gracefully handle
+    // empty strings as labels. 
+    private String label = " ";
 
     public ChatZonesCell(CellID cellID, CellCache cellCache) {
         super(cellID, cellCache);
@@ -81,38 +87,12 @@ public class ChatZonesCell extends Cell {
             listener.removeFromEntity(renderer.getEntity());
             listener = null;
             
+        } else if (status==CellStatus.RENDERING&& !increasing) {
+            // As we're falling down the status chain, try removing the listener
+            // earlier. It seems to be gone by the time we get to DISK.
             channel.removeMessageReceiver(ChatZonesCellChangeMessage.class);
         }
 
-
-//        switch(status) {
-//            case DISK:
-//                if(listener != null) {
-//                    listener.removeFromEntity(renderer.getEntity());
-//                    listener = null;
-//
-//                    logger.info("Cell getting set to DISK.");
-//                }
-//                if(!increasing) channel.removeMessageReceiver(ChatZonesCellChangeMessage.class);
-//                break;
-//            case ACTIVE:
-//
-//                labelDialog = new ChatZoneLabelDialog(this);
-////                logger.warning("MADE A NEW LABEL DIALOG BOX");
-//
-//                if(listener==null) {
-//                    listener =  new MouseEventListener(labelDialog);
-//                    listener.addToEntity(renderer.getEntity());
-//
-//                    logger.info("Making cell active and added a new listener: " + listener);
-//                }
-//                if(increasing) channel.addMessageReceiver(ChatZonesCellChangeMessage.class, new ChatZonesCellMessageReceiver());
-//
-//                break;
-//
-//            default:
-//                break;
-//        }
     }
 
     public void setLabel(String newLabel) {
@@ -158,6 +138,7 @@ public class ChatZonesCell extends Cell {
                         logger.warning("avatar count is out of sync with server, syncing: " +bsccm.getNumAvatarInZone() + " -> " + numAvatarsInZone);
                         numAvatarsInZone = bsccm.getNumAvatarInZone();
                     }
+                    ClientContextJME.getWorldManager().addRenderUpdater(new SizeRenderUpdater(), null);
                     break;
                 case LEFT:
                     numAvatarsInZone--;
@@ -166,6 +147,7 @@ public class ChatZonesCell extends Cell {
                         logger.warning("avatar count is out of sync with server, syncing: " +bsccm.getNumAvatarInZone() + " -> " + numAvatarsInZone);
                         numAvatarsInZone = bsccm.getNumAvatarInZone();
                     }
+                    ClientContextJME.getWorldManager().addRenderUpdater(new SizeRenderUpdater(), null);
                     break;
                 case LABEL:
                     logger.warning("Changed cell name to: " + bsccm.getLabel());
@@ -200,11 +182,20 @@ public class ChatZonesCell extends Cell {
         public void commitEvent(Event event) {
             MouseButtonEvent3D mbe = (MouseButtonEvent3D)event;
 
-            logger.info("Got click! " + event);
-//            SwingUtilities.invokeLater(new Thread() {
-//                labelDialog.setvisible(true);
-//            });
-            labelDialog.setVisible(true);
+            // Filter out right mouse clicks.
+            if(mbe.getButton() == MouseButtonEvent3D.ButtonId.BUTTON1) {
+                logger.info("Got click! " + event);
+                labelDialog.setVisible(true);
+            }
+        }
+
+    }
+
+    public class SizeRenderUpdater implements RenderUpdater {
+
+        public void update(Object arg0) {
+            logger.warning("In SIZE RENDER UPDATER");
+            renderer.updateSize(numAvatarsInZone);
         }
 
     }
