fa7669c9e508ba55301cf2a34413a82d0d14058c
kmontag <kmontag@df51345c-241b-11df-8743-957545a88a67>
2009-07-22 00:07:58 +0000

    Added defaults file needed for compilation.

---
+++ 0.5/unstable/cmu-module/src/classes/org/jdesktop/wonderland/modules/cmu/common/cell/PlaybackDefaults.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jdesktop.wonderland.modules.cmu.common.cell;
+
+/**
+ *
+ * @author kevin
+ */
+public class PlaybackDefaults {
+    public static final float DEFAULT_PLAYBACK_SPEED = 1.0f;
+    public static final float PAUSE_SPEED = 0.0f;
+    public static final float DEFAULT_START_SPEED = PAUSE_SPEED;
+}
