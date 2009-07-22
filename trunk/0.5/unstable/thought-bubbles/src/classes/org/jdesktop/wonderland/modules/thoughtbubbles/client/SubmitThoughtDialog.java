3bc06169e73aa979e91a057f81b193efc677fc3e
drew_harry <drew_harry@df51345c-241b-11df-8743-957545a88a67>
2009-07-22 19:19:16 +0000

    Build changes to require presentation-base, as well as logic to place a button on the presentation-base toolbar when users are in a cell taht supports thought bubbles.

--- trunk/0.5/unstable/thought-bubbles/src/classes/org/jdesktop/wonderland/modules/thoughtbubbles/client/SubmitThoughtDialog.java
+++ trunk/0.5/unstable/thought-bubbles/src/classes/org/jdesktop/wonderland/modules/thoughtbubbles/client/SubmitThoughtDialog.java
@@ -1,19 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * SubmitThoughtDialog.java
+/**
+ * Project Wonderland
  *
- * Created on Jul 21, 2009, 4:55:58 PM
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
  */
 
 package org.jdesktop.wonderland.modules.thoughtbubbles.client;
 
 /**
+ * Dialog box for collecting a thought from a user. Meant to appeear
+ * on the HUD.
  *
- * @author drew
+ * @author Drew Harry <drew_harry@dev.java.net>
  */
 public class SubmitThoughtDialog extends javax.swing.JPanel {
 
