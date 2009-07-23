fb3907a2aea595b3e16aa5d24ca067caf5e5f483
morrisford <morrisford@df51345c-241b-11df-8743-957545a88a67>
2009-07-23 15:56:03 +0000

    corrected some mistakes

--- trunk/0.5/unstable/scriptingComponent/src/classes/org/jdesktop/wonderland/modules/scriptingComponent/client/TridentAnimations.java
+++ trunk/0.5/unstable/scriptingComponent/src/classes/org/jdesktop/wonderland/modules/scriptingComponent/client/TridentAnimations.java
@@ -5,6 +5,9 @@
 package org.jdesktop.wonderland.modules.scriptingComponent.client;
 
 import org.pushingpixels.trident.Timeline;
+import org.pushingpixels.trident.TimelineScenario;
+import org.pushingpixels.trident.ease.Spline;
+import org.pushingpixels.trident.interpolator.PropertyInterpolator;
 
 /**
  *
@@ -13,28 +16,61 @@ import org.pushingpixels.trident.Timeline;
 public class TridentAnimations
     {
     private float value;
+    private ScriptingComponent sc;
 
+    public TridentAnimations(ScriptingComponent SC)
+        {
+        this.sc = SC;
+        }
+    public TridentAnimations()
+        {
+
+        }
     public void setValue(float newValue)
         {
-        System.out.println(this.value + " -> " + newValue);
         this.value = newValue;
+        sc.setTranslation(newValue * 2, 0, 0, 1);
         }
 
-    public void go()
+    public void go(ScriptingComponent SC)
         {
-        System.out.println("Enter TridentAnimations.go()");
-        TridentAnimations tt = new TridentAnimations();
-        Timeline timeline = new Timeline(tt);
-        timeline.addPropertyToInterpolate("value", 0.0f, 1.0f);
-        timeline.play();
+        System.out.println("Enter TridentAnimations.go() - scripting comp = " + SC);
 
-        try
-            {
-            Thread.sleep(3000);
-            }
-        catch (Exception exc)
-            {
-            }
+        TridentAnimations tt = new TridentAnimations(SC);
+        Timeline timeline = new Timeline(tt);
+        timeline.addPropertyToInterpolate("value", 0.0f, 1.0f, new FloatPropertyInterpolator());
+        timeline.setEase(new Spline(0.4f));
+        timeline.play();
         }
 
+    public void go1(ScriptingComponent SC)
+        {
+        TridentAnimations tt = new TridentAnimations(SC);
+        TimelineScenario result = new TimelineScenario.Sequence();
+        Timeline t = new Timeline(tt);
+        t.addPropertyToInterpolate("value", 0.0f, 1.0f, new FloatPropertyInterpolator());
+        t.setEase(new Spline(0.4f));
+        t.setDuration(2000);
+        result.addScenarioActor(t);
+
+        Timeline u = new Timeline(tt);
+        u.addPropertyToInterpolate("value", 1.0f, 0.0f, new FloatPropertyInterpolator());
+        u.setDuration(4000);
+        result.addScenarioActor(u);
+        result.play();
+        }
+    }
+
+class FloatPropertyInterpolator implements PropertyInterpolator<Float>
+    {
+
+    public Class getBasePropertyClass()
+        {
+        return Float.class;
+        }
+
+    public Float interpolate(Float from, Float to, float timelinePosition)
+        {
+        return from +(to - from) * timelinePosition;
+        }
     }
