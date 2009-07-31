d0805351618310299e80682f7c43f69b55b46061
drew_harry <drew_harry@df51345c-241b-11df-8743-957545a88a67>
2009-07-31 19:14:28 +0000

    Initial checkin of the bits that will make up the moving presentation platform. Doesn't do anything now, except show up in world as a big flat platform.

---
+++ trunk/0.5/unstable/presentation-base/src/classes/org/jdesktop/wonderland/modules/presentationbase/client/MovingPlatformCell.java
@@ -0,0 +1,54 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
+ */
+
+package org.jdesktop.wonderland.modules.presentationbase.client;
+
+import java.util.logging.Logger;
+import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.cell.CellCache;
+import org.jdesktop.wonderland.client.cell.CellRenderer;
+import org.jdesktop.wonderland.common.cell.CellID;
+import org.jdesktop.wonderland.modules.presentationbase.client.jme.cell.MovingPlatformCellRenderer;
+
+/**
+ *
+ * @author Drew Harry <drew_harry@dev.java.net
+ */
+public class MovingPlatformCell extends Cell {
+    
+    MovingPlatformCellRenderer renderer = null;
+
+    private static final Logger logger =
+            Logger.getLogger(MovingPlatformCell.class.getName());
+
+    public MovingPlatformCell(CellID cellID, CellCache cellCache) {
+        super(cellID, cellCache);
+    }
+
+    @Override
+    protected CellRenderer createCellRenderer(RendererType rendererType) {
+        if (rendererType == RendererType.RENDERER_JME) {
+            this.renderer = new MovingPlatformCellRenderer(this);
+            return this.renderer;
+        }
+        else {
+            return super.createCellRenderer(rendererType);
+        }
+    }
+
+}
