888388adc28f07f22a12c01c6a10c9f365d4109f
drew_harry <drew_harry@df51345c-241b-11df-8743-957545a88a67>
2009-08-10 23:26:18 +0000

    Spiral movement is basically working, although you have to turn avatar gravity off or the avatr will instantly get sucked down to the ground plane.

--- 0.5/unstable/timeline/src/classes/org/jdesktop/wonderland/modules/timeline/client/TimelineCell.java
+++ 0.5/unstable/timeline/src/classes/org/jdesktop/wonderland/modules/timeline/client/TimelineCell.java
@@ -60,7 +60,23 @@ public class TimelineCell extends Cell implements ProximityListener {
     private HUDComponent navigationHUD;
 
     private AvatarCell localAvatarCell;
-    
+
+    /**
+     * The pitch of the helix (which is the vertical distance of one complete
+     * turn).
+     */
+    private float pitch = 2.0f;
+
+    /**
+     * The height in meters of the helix. (There are lots of alternative ways
+     * to represent this - could be the number of turns or just the raw number
+     * of radians the helix covers. They're interchangeable, though, so I just
+     * picked the one that seems the most accessible to users. If we want to
+     * switch go something that is easier for all of us to work with internally,
+     * that's super easy to do.)
+     */
+    private float height = 30.0f;
+
     public TimelineCell(CellID cellID, CellCache cellCache) {
         super(cellID, cellCache);
     }
@@ -71,7 +87,7 @@ public class TimelineCell extends Cell implements ProximityListener {
         
         if(status==CellStatus.ACTIVE && increasing) {
 
-            this.setLocalBounds(new BoundingBox(Vector3f.ZERO, 10.0f, 10.0f, 10.0f));
+            this.setLocalBounds(new BoundingBox(Vector3f.ZERO, 100.0f, 100.0f, 100.0f));
 
             BoundingVolume[] bounds = new BoundingVolume[]{this.getLocalBounds()};
             prox.addProximityListener(this, bounds);
@@ -91,8 +107,7 @@ public class TimelineCell extends Cell implements ProximityListener {
                 // Add the navigation HUD.
                 mainHUD = HUDManagerFactory.getHUDManager().getHUD("main");
 
-                navigationHUD = mainHUD.createComponent(new TimelineMovementHUDPanel(this));
-                
+                navigationHUD = mainHUD.createComponent(new TimelineMovementHUDPanel(this));                
                 navigationHUD.setPreferredLocation(Layout.EAST);
                 navigationHUD.setName("Navigation");
 
@@ -138,11 +153,37 @@ public class TimelineCell extends Cell implements ProximityListener {
      */
     public void moveAvatar(float position) {
 
+        // Move the avatar on their current radius; ie no need to detect which
+        // layer/track they're on, just pick their current distance to 0,0
+        // and use that as the radius. 
 
-        Vector3f targetPosition = this.getWorldTransform().getTranslation(null).add(new Vector3f(0, 0, 20 * position));
+        Vector3f avPosition = localAvatarCell.getWorldTransform().getTranslation(Vector3f.ZERO);
 
-        logger.warning("Moving avatar to position: " + targetPosition + " (" + position + ")");
+        Vector3f cellPositionAtHeight = this.getWorldTransform().getTranslation(null);
 
+        // Set the heights the same so we can get just the distance to the center, not the
+        // distance to the actual center of the cell.
+        cellPositionAtHeight.y = avPosition.y;
+        
+        float radius = avPosition.distance(cellPositionAtHeight);
+
+        // Given the fraction up the helix we want to be (position), figure out
+        // what the angle (t) of that is.
+
+        // Since the height = pitch*t, just divide it to get the number of turns,
+        // and the angle is 2PI times that.
+        float targetHeight = (position * height);
+        float angle = (float) ((float) (targetHeight / (float)pitch) * 2.0f * Math.PI);
+
+        Vector3f positionRelativeToCell = new Vector3f((float)(radius * Math.sin(angle)), targetHeight, (float)(radius * Math.cos(angle)));
+
+        // This is definitely not quite right - the height of the cell is not
+        // interesting to us and we should be knocking it out. But we do care
+        // about x,z positions, since the avatar isn't a child of the timeline
+        // and so needs to have its world positions set.
+        Vector3f targetPosition = this.getWorldTransform().getTranslation(null).add(positionRelativeToCell);
+
+        logger.warning("r=" + radius + "; moving avatar to position: " + targetPosition + " (" + position + ")");
 
         localAvatarCell.triggerGoto(targetPosition, new Quaternion());
     }
