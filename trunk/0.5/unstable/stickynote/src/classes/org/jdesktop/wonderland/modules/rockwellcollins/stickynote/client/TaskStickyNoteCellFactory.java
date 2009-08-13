dd5e449631281dda19859953ff950ff6ddce245a
mymegabyte <mymegabyte@df51345c-241b-11df-8743-957545a88a67>
2009-08-13 20:21:40 +0000

    Initial import of an in world "Sticky Note" module. This module creates two types of sticky notes that can be added. 1) Generic sticky note is a yellow scrollable text box. 2) Task sticky note is a form based note with purpose of tracking to-do items or the status of tasks. The task note has two display states, collapsed and expanded. The collapsed state shows only the task name. The expanded state shows all the details, but only expands the note for the local user. (Not a world-visible change). This module used the swing example program and svg whiteboard as examples to build off of (and borrow code).

---
+++ trunk/0.5/unstable/stickynote/src/classes/org/jdesktop/wonderland/modules/rockwellcollins/stickynote/client/TaskStickyNoteCellFactory.java
@@ -0,0 +1,63 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.rockwellcollins.stickynote.client;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.jdesktop.wonderland.client.cell.registry.annotation.CellFactory;
+import org.jdesktop.wonderland.client.cell.registry.spi.CellFactorySPI;
+import org.jdesktop.wonderland.common.cell.state.CellServerState;
+import org.jdesktop.wonderland.modules.rockwellcollins.stickynote.common.cell.StickyNoteCellServerState;
+import org.jdesktop.wonderland.modules.rockwellcollins.stickynote.common.cell.StickyNoteTypes;
+
+/**
+ * The task sticky note factory.
+ * @author Ryan (mymegabyte)
+ */
+@CellFactory
+public class TaskStickyNoteCellFactory implements CellFactorySPI {
+
+    public String[] getExtensions() {
+        return new String[]{};
+    }
+
+    public <T extends CellServerState> T getDefaultCellServerState(Properties props) {
+        StickyNoteCellServerState state = new StickyNoteCellServerState();
+        state.setNoteType(StickyNoteTypes.TASK);
+        state.setColor("255:255:255");
+        // HACK!
+        Map<String, String> metadata = new HashMap();
+        metadata.put("sizing-hint", "2.0");
+        state.setMetaData(metadata);
+
+        return (T) state;
+    }
+
+    public String getDisplayName() {
+        return "Task Sticky Note";
+    }
+
+    public Image getPreviewImage() {
+        URL url = TaskStickyNoteCellFactory.class.getResource("resources/task_stickynote_preview.png");
+        return Toolkit.getDefaultToolkit().createImage(url);
+    }
+}
