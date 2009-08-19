9273b914d24fa0aa7e17c4a702b1b9d16cf651ee
kmontag <kmontag@df51345c-241b-11df-8743-957545a88a67>
2009-08-19 01:48:18 +0000

    Deploy and run player directly from server

---
+++ trunk/0.5/unstable/cmu-module/src/classes/org/jdesktop/wonderland/modules/cmu/player/VisualUploader.java
@@ -0,0 +1,49 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.cmu.player;
+
+/**
+ *
+ * @author kevin
+ */
+public class VisualUploader {
+/*
+    static private ServerSessionManager manager;
+    static private boolean initialized = false;
+
+    // Should never be instantiated
+    private VisualUploader() {
+    }
+
+    static public void initialize(ServerSessionManager manager) {
+        WebdavClientPlugin plugin = new WebdavClientPlugin();
+        plugin.initialize(manager);
+        VisualUploader.manager = manager;
+        initialized = true;
+    }
+
+    static public void uploadVisual(VisualWrapper visual) {
+        if (initialized) {
+            try {
+                ContentCollection collection = ContentRepositoryRegistry.getInstance().getRepository(manager).getUserRoot();
+            } catch (ContentRepositoryException ex) {
+                Logger.getLogger(VisualUploader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }*/
+}
