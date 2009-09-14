85caa4a3936b3c75c60d76b3eb2f61f9341784cc
bernard_horan <bernard_horan@df51345c-241b-11df-8743-957545a88a67>
2009-09-14 11:51:04 +0000

    Add NPC to replay avatars. WIP, breaks with >1 avatar.

---
+++ 0.5/unstable/eventplayer-module/src/classes/org/jdesktop/wonderland/modules/eventplayer/client/npcplayer/MovableNpcPlayerComponent.java
@@ -0,0 +1,57 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.eventplayer.client.npcplayer;
+
+import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.cell.MovableAvatarComponent;
+import org.jdesktop.wonderland.client.cell.MovableComponent;
+import org.jdesktop.wonderland.client.cell.TransformChangeListener;
+import org.jdesktop.wonderland.common.cell.CellTransform;
+import org.jdesktop.wonderland.common.cell.ComponentLookupClass;
+import org.jdesktop.wonderland.common.cell.messages.MovableAvatarMessage;
+import org.jdesktop.wonderland.common.cell.messages.MovableMessage;
+
+/**
+ * A component that extends MovableAvatarComponent to add additional information
+ * for animating avatars. We can't use MovableAvatarComponent here because it explicitly casts cell to
+ * AvatarCell. Adapted from the NPC MovableNpcPlayerComponent written by Paul Byrne.
+ *
+ * @author Bernard Horan
+ */
+@ComponentLookupClass(MovableComponent.class)
+public class MovableNpcPlayerComponent extends MovableAvatarComponent {
+    public MovableNpcPlayerComponent(Cell cell) {
+        super(cell);
+    }
+
+
+    @Override
+    protected void serverMoveRequest(MovableMessage msg) {
+        CellTransform transform = msg.getCellTransform();
+        applyLocalTransformChange(transform, TransformChangeListener.ChangeSource.REMOTE);
+        notifyServerCellMoveListeners(msg, transform, CellMoveSource.REMOTE);
+
+        MovableAvatarMessage mam = (MovableAvatarMessage) msg;
+        //System.err.println("Move message "+msg.getCellTransform().getTranslation(null)+"  "+mam.getTrigger()+" "+mam.getAnimationName());
+        if (mam.getTrigger()!=NO_TRIGGER) {
+            ((NpcPlayerCell)cell).triggerAction(mam.getTrigger(), mam.isPressed(), mam.getAnimationName());
+        }
+    }
+
+
+}
---
+++ 0.5/unstable/eventplayer-module/src/classes/org/jdesktop/wonderland/modules/eventplayer/server/npcplayer/NpcPlayerCellMO.java
@@ -0,0 +1,115 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.eventplayer.server.npcplayer;
+
+import org.jdesktop.wonderland.common.cell.state.CellServerState;
+import com.sun.sgs.app.ManagedReference;
+import org.jdesktop.wonderland.common.cell.ClientCapabilities;
+import org.jdesktop.wonderland.common.cell.messages.CellMessage;
+import org.jdesktop.wonderland.common.cell.state.CellClientState;
+import org.jdesktop.wonderland.modules.eventplayer.common.npcplayer.NpcPlayerCellChangeMessage;
+import org.jdesktop.wonderland.modules.eventplayer.common.npcplayer.NpcPlayerCellClientState;
+import org.jdesktop.wonderland.modules.eventplayer.common.npcplayer.NpcPlayerCellServerState;
+import org.jdesktop.wonderland.server.cell.AbstractComponentMessageReceiver;
+import org.jdesktop.wonderland.server.cell.CellMO;
+import org.jdesktop.wonderland.server.cell.ChannelComponentMO;
+import org.jdesktop.wonderland.server.cell.annotation.UsesCellComponentMO;
+import org.jdesktop.wonderland.server.comms.WonderlandClientID;
+import org.jdesktop.wonderland.server.comms.WonderlandClientSender;
+
+/**
+ * The server-side Cell MO for the NPC Player Cell.<br>
+ * Adapted from the NPC module.
+ * 
+ * @author paulby
+ * @author david <dmaroto@it.uc3m.es> UC3M - "Project España Virtual"
+ * @author Jordan Slott (jslott@dev.java.net)
+ * @author Bernard Horan
+ */
+public class NpcPlayerCellMO extends CellMO {
+
+    @UsesCellComponentMO(MovableNpcPlayerComponentMO.class)
+    private ManagedReference<MovableNpcPlayerComponentMO> npcMovableComp;
+
+//    @UsesCellComponentMO(NpcAvatarConfigComponentMO.class)
+//    private ManagedReference<NpcAvatarConfigComponentMO> avatarConfigComp;
+    
+    // The relative avatar configuration URL
+    private String relativeConfigURL = null;
+
+    /** Default constructor */
+    public NpcPlayerCellMO() {
+    }
+
+    /**
+     * Returns the URL of the avatar configuration, relative to the module.
+     * 
+     * @return The relative URL of the avatar configuration file
+     */
+    public String getRelativeConfigURL() {
+        return relativeConfigURL;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected String getClientCellClassName(WonderlandClientID clientID,
+                                            ClientCapabilities capabilities) {
+        return "org.jdesktop.wonderland.modules.eventplayer.client.npcplayer.NpcPlayerCell";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CellServerState getServerState(CellServerState state) {
+        // Create an appropriate NPC server state if one does not exist
+        if (state == null) {
+            state = new NpcPlayerCellServerState();
+        }
+        return super.getServerState(state);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setServerState(CellServerState state) {
+        super.setServerState(state);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CellClientState getClientState(CellClientState state,
+            WonderlandClientID clientID, ClientCapabilities capabilities) {
+
+        // Create a new client state if one does not exist
+        if (state == null) {
+            state = new NpcPlayerCellClientState();
+        }
+        return super.getClientState(state, clientID, capabilities);
+    }
+
+    
+
+    
+}
