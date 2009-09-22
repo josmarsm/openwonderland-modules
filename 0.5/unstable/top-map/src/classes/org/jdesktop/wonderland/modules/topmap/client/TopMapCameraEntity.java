bf286b1c30220fb70cc11737240b5aa1effbbd30
jslott <jslott@df51345c-241b-11df-8743-957545a88a67>
2009-09-22 22:04:19 +0000

    Initial commit of unstable module "top map" that displays a map view of the world from the avatar's perspective in the HUD. DISCLAIMER: This is an initial commit, it is PRIMARILY meant to support writing a tutorial about the HUD and will not be developed into stable-like functionality in the near future.
    Among the items that still need to be worked out with the code that is there:
    - Needs to wait for the primary view cell, rather than activate() in the client plugin-in
    - Needs to have the parallel view
    - Map should turn as the avatar turns
    - Seems to slow down the client quite a bit -- best way to capture the images?
    - Need a GUI to control the height/frustum of the camera, needed for the tutorial

---
+++ 0.5/unstable/top-map/src/classes/org/jdesktop/wonderland/modules/topmap/client/TopMapCameraEntity.java
@@ -0,0 +1,169 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.topmap.client;
+
+import com.jme.math.Quaternion;
+import com.jme.math.Vector3f;
+import com.jme.scene.CameraNode;
+import com.jme.scene.Node;
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+import org.jdesktop.mtgame.Entity;
+import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
+import org.jdesktop.mtgame.CameraComponent;
+import org.jdesktop.mtgame.RenderBuffer;
+import org.jdesktop.mtgame.RenderManager;
+import org.jdesktop.mtgame.RenderUpdater;
+import org.jdesktop.mtgame.TextureRenderBuffer;
+import org.jdesktop.mtgame.WorldManager;
+import org.jdesktop.mtgame.processor.WorkProcessor.WorkCommit;
+import org.jdesktop.wonderland.client.cell.TransformChangeListener;
+import org.jdesktop.wonderland.client.cell.view.ViewCell;
+import org.jdesktop.wonderland.client.jme.SceneWorker;
+import org.jdesktop.wonderland.client.jme.ViewManager;
+import org.jdesktop.wonderland.common.cell.CellTransform;
+
+/**
+ * An Entity that contains a Camera that can be attached to the world and that
+ * can track the movements of a Cell.
+ *
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+public class TopMapCameraEntity extends Entity implements RenderUpdater {
+
+    // The buffer into which the camera is renderer, so we can copy it into
+    // a BufferedImage
+    private TextureRenderBuffer textureBuffer = null;
+
+    // The JComponent into which the camera scene should be drawn
+    private CaptureJComponent captureComponent = null;
+
+    // The scene graph node for the position of the camera
+    private Node cameraNode = null;
+
+    /**
+     * Default constructor, takes the JComponent into which it should draw
+     * the camera scene.
+     */
+    public TopMapCameraEntity(CaptureJComponent captureComponent) {
+        super("Top Camera Entity");
+        this.captureComponent = captureComponent;
+
+        // Creates the scene graph and attach to the entity
+        createTopMap();
+    }
+
+
+    /**
+     * Creates the scene graph for the camera map Entity.
+     */
+    private void createTopMap() {
+
+        // Fetch the world and render managers for use
+        final WorldManager wm = ClientContextJME.getWorldManager();
+        final RenderManager rm = wm.getRenderManager();
+
+        // Create a buffer into which the camera scene is renderer
+        BufferedImage bufferedImage = captureComponent.getBufferedImage();
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        textureBuffer = (TextureRenderBuffer) rm.createRenderBuffer(
+                RenderBuffer.Target.TEXTURE_2D, width, height);
+        textureBuffer.setIncludeOrtho(false);
+        
+        // Create the new camera and a node to hold it. We attach this to the
+        // Entity
+        cameraNode = new Node();
+        cameraNode.setLocalTranslation(0.0f, 10.0f, 0.0f);
+        Quaternion rot = new Quaternion().fromAngleAxis(90.0f, Vector3f.UNIT_X);
+        cameraNode.setLocalRotation(rot);
+        CameraNode cn = new CameraNode("Top Camera", null);
+        cameraNode.attachChild(cn);
+
+        // Create a camera component and associated with the texture buffer we
+        // have created.
+        CameraComponent cc = rm.createCameraComponent(cameraNode, cn, width,
+                height, 90.0f, 1.0f, 0.1f, 10000.0f, false);
+        textureBuffer.setCameraComponent(cc);
+        rm.addRenderBuffer(textureBuffer);
+        textureBuffer.setRenderUpdater(this);
+
+        ViewCell viewCell = ViewManager.getViewManager().getPrimaryViewCell();
+        if (viewCell != null) {
+            viewCell.addTransformChangeListener(new TransformChangeListener() {
+                public void transformChanged(Cell cell, ChangeSource source) {
+                    
+                   CellTransform transform = cell.getWorldTransform();
+                   final Vector3f translation = transform.getTranslation(null);
+                   final Quaternion rotation = transform.getRotation(null);
+
+                   SceneWorker.addWorker(new WorkCommit() {
+                        public void commit() {
+                            float x = translation.getX();
+                            float y = translation.getY() + 10.0f;
+                            float z = translation.getZ();
+                            cameraNode.setLocalTranslation(x, y, z);
+                            wm.addToUpdateList(cameraNode);
+
+//                            Quaternion q = cameraNode.getLocalRotation();
+//                            cameraNode.setLocalRotation(q.mult(rotation));
+                            wm.addToUpdateList(cameraNode);
+                        }
+                    });
+                }
+            });
+        }
+
+        // Add the camera component to the Entity
+        addComponent(CameraComponent.class, cc);
+    }
+
+    /**
+     * Takes a BufferedImage and a ByteBuffer and fills the values found in the
+     * byte buffer into the buffered image, assuming consecutive RGB values.
+     */
+    private void fill(BufferedImage bi, ByteBuffer bb, int width, int height) {
+        bb.rewind();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int index = (y * width + x) * 3;
+                int b = bb.get(index);
+                int g = bb.get(index + 1);
+                int r = bb.get(index + 2);
+
+                int pixel = ((r & 255) << 16) | ((g & 255) << 8) |
+                        ((b & 255)) | 0xff000000;
+                bi.setRGB(x, (height - y) - 1, pixel);
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void update(Object arg0) {
+        // Take the latest from the texture buffer into which we rendered
+        // the camera and draw it into the buffered image and tell the
+        // JComponent to repaint itself.
+        BufferedImage bi = captureComponent.getBufferedImage();
+        ByteBuffer bb = textureBuffer.getTextureData();
+        fill(bi, bb, bi.getWidth(), bi.getHeight());
+        captureComponent.repaint();
+    }
+}
---
+++ 0.5/unstable/top-map/src/classes/org/jdesktop/wonderland/modules/topmap/client/TopMapClientPlugin.java
@@ -0,0 +1,153 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.topmap.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.ref.WeakReference;
+import java.util.ResourceBundle;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
+import org.jdesktop.mtgame.WorldManager;
+import org.jdesktop.wonderland.client.BaseClientPlugin;
+import org.jdesktop.wonderland.client.hud.HUDEvent;
+import org.jdesktop.wonderland.client.jme.JmeClientMain;
+import org.jdesktop.wonderland.client.login.ServerSessionManager;
+import org.jdesktop.wonderland.common.annotation.Plugin;
+import org.jdesktop.wonderland.client.hud.CompassLayout.Layout;
+import org.jdesktop.wonderland.client.hud.HUD;
+import org.jdesktop.wonderland.client.hud.HUDComponent;
+import org.jdesktop.wonderland.client.hud.HUDEvent.HUDEventType;
+import org.jdesktop.wonderland.client.hud.HUDEventListener;
+import org.jdesktop.wonderland.client.hud.HUDManagerFactory;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
+
+/**
+ * Client-side plugin for the top map.
+ * 
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+@Plugin
+public class TopMapClientPlugin extends BaseClientPlugin {
+
+    // The I18N resource bundle
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(
+            "org/jdesktop/wonderland/modules/topmap/client/resources/Bundle");
+
+    // The top map menu item to add to the Windows menu
+    private JMenuItem topMapMI = null;
+
+    // The HUD Component displaying the top map, stored as a weak reference
+    // to help GC
+    private WeakReference<HUDComponent> hudRef = null;
+
+    // Create a new top camera Entity used to capture the scene
+    private TopMapCameraEntity topMapEntity = null;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(ServerSessionManager loginInfo) {
+        // Create the Top Map menu item. Upon activation, create the HUD window
+        // that displays the map.
+        topMapMI = new JCheckBoxMenuItem(BUNDLE.getString("Top_Map"));
+        topMapMI.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Depending upon whether the checkbox is selected or not,
+                // either hide or show the HUD Component
+                if (topMapMI.isSelected() == true) {
+                    if (hudRef == null || hudRef.get() == null) {
+                        HUDComponent hudComponent = createHUD();
+                        hudRef = new WeakReference<HUDComponent>(hudComponent);
+                    }
+                    HUDComponent hudComponent = hudRef.get();
+                    hudComponent.setVisible(true);
+                }
+                else {
+                    HUDComponent hudComponent = hudRef.get();
+                    if (hudComponent != null) {
+                        hudComponent.setVisible(false);
+                    }
+                }
+            }
+        });
+
+        super.initialize(loginInfo);
+    }
+
+    /**
+     * Creates and returns the top map HUD component.
+     */
+    private HUDComponent createHUD() {
+        // Create the HUD Panel that displays the map.
+        HUD mainHUD = HUDManagerFactory.getHUDManager().getHUD("main");
+        TopMapJPanel panel = new TopMapJPanel();
+        HUDComponent hudComponent = mainHUD.createComponent(panel);
+        hudComponent.setName(BUNDLE.getString("Top_Map_Title"));
+        hudComponent.setPreferredLocation(Layout.SOUTHEAST);
+        mainHUD.addComponent(hudComponent);
+
+        // Track when the HUD Component is closed. We need to update the state
+        // of the check box menu item too
+        hudComponent.addEventListener(new HUDEventListener() {
+            public void HUDObjectChanged(HUDEvent event) {
+                if (event.getEventType() == HUDEventType.CLOSED) {
+                    topMapMI.setSelected(false);
+                }
+            }
+        });
+
+        // Create the Entity that holds the camera and add it to the world
+        topMapEntity = new TopMapCameraEntity(panel.getCaptureJComponent());
+        WorldManager wm = ClientContextJME.getWorldManager();
+        wm.addEntity(topMapEntity);
+        
+        return hudComponent;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void activate() {
+        JmeClientMain.getFrame().addToWindowMenu(topMapMI, -1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void deactivate() {
+        JmeClientMain.getFrame().removeFromWindowMenu(topMapMI);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void cleanup() {
+        // If there is a HUD Component, then remove it from the HUD and clean
+        // it up.
+        HUDComponent hudComponent = hudRef.get();
+        if (hudComponent != null) {
+            HUD mainHUD = HUDManagerFactory.getHUDManager().getHUD("main");
+            mainHUD.removeComponent(hudComponent);
+        }
+    }
+}
