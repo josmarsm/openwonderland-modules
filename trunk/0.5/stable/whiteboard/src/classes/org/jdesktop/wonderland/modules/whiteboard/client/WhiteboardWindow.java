54f3b776c2e618dbeb9c60621105826a2d54d9c7
nigel@matsuplace.com <nigel@matsuplace.com@df51345c-241b-11df-8743-957545a88a67>
2010-07-02 18:21:47 +0000

    Display control panel on HUD y default.

--- trunk/0.5/stable/whiteboard/src/classes/org/jdesktop/wonderland/modules/whiteboard/client/WhiteboardWindow.java
+++ trunk/0.5/stable/whiteboard/src/classes/org/jdesktop/wonderland/modules/whiteboard/client/WhiteboardWindow.java
@@ -1,4 +1,21 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2010, Sun Microsystems, Inc., All Rights Reserved
@@ -130,7 +147,7 @@ public class WhiteboardWindow extends Window2D {
         setTitle(BUNDLE.getString("Whiteboard"));
         initCanvas(width, height);
         initHUD();
-        setDisplayMode(DisplayMode.WORLD);
+        setDisplayMode(DisplayMode.HUD);
         showControls(false);
         wbSurface = (WhiteboardDrawingSurface) getSurface();
 
