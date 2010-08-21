edab53f8b1b72137f0186168d199f19ce1207eab
jonathankap <jonathankap@df51345c-241b-11df-8743-957545a88a67>
2010-08-21 18:26:15 +0000

    Updates to work with separate internal / external IP addresses

--- trunk/0.5/stable/audiorecorder-module/src/classes/org/jdesktop/wonderland/modules/audiorecorder/server/AudioRecorderCellMO.java
+++ trunk/0.5/stable/audiorecorder-module/src/classes/org/jdesktop/wonderland/modules/audiorecorder/server/AudioRecorderCellMO.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2010, Sun Microsystems, Inc., All Rights Reserved
@@ -207,7 +225,15 @@ public class AudioRecorderCellMO extends CellMO implements ManagedCallStatusList
     private void startPlaying(Tape aTape) {
         audioRecorderLogger.info("Start Playing: " + aTape.getURL().toString());
         try {
-            recorder.playRecording(aTape.getURL().toString());
+            // convert from wlcontent:// URL to a local URL
+            String urlStr = aTape.getURL().toString();
+            if (urlStr.startsWith("wlcontent://")) {
+                urlStr = urlStr.substring("wlcontent://".length());
+                urlStr = System.getProperty("wonderland.web.server.url") +
+                         "webdav/content/" + urlStr;
+            }
+            
+            recorder.playRecording(urlStr);
         } catch (IOException e) {
             System.out.println(e);
         }
