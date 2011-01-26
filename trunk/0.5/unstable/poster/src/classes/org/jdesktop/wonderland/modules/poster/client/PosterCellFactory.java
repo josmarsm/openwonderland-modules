4db4aa304f3227702c6f436d2b329e4fb4d68965
bernhoran@gmail.com <bernhoran@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2011-01-26 09:43:53 +0000

    Reworking of poster rendering to use swing app. This improves the rendering of text.

--- trunk/0.5/unstable/poster/src/classes/org/jdesktop/wonderland/modules/poster/client/PosterCellFactory.java
+++ trunk/0.5/unstable/poster/src/classes/org/jdesktop/wonderland/modules/poster/client/PosterCellFactory.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2011, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2010, Sun Microsystems, Inc., All Rights Reserved
@@ -43,6 +61,7 @@ public class PosterCellFactory implements CellFactorySPI {
         return new String[]{};
     }
 
+    @SuppressWarnings("unchecked")
     public <T extends CellServerState> T getDefaultCellServerState(Properties props) {
         CellServerState state = new PosterCellServerState();
         BoundingSphere box = new BoundingSphere(1.0f, new Vector3f(1f, 0.7f, 0.2f));
--- trunk/0.5/unstable/poster/src/classes/org/jdesktop/wonderland/modules/poster/common/PosterCellServerState.java
+++ trunk/0.5/unstable/poster/src/classes/org/jdesktop/wonderland/modules/poster/common/PosterCellServerState.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2011, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2010, Sun Microsystems, Inc., All Rights Reserved
@@ -19,9 +37,11 @@ package org.jdesktop.wonderland.modules.poster.common;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import org.jdesktop.wonderland.common.cell.state.CellServerState;
+import javax.xml.bind.annotation.XmlTransient;
 import org.jdesktop.wonderland.common.cell.state.annotation.ServerState;
+import org.jdesktop.wonderland.modules.appbase.common.cell.App2DCellServerState;
 
 /**
  * The cell server state for the PosterCell.<br>
@@ -35,7 +55,14 @@ import org.jdesktop.wonderland.common.cell.state.annotation.ServerState;
 // to XML unless annotated with @XmlTransient
 @XmlAccessorType(XmlAccessType.FIELD)
 @ServerState
-public class PosterCellServerState extends CellServerState {
+public class PosterCellServerState extends App2DCellServerState {
+    /** The user's preferred width of the poll window. */
+    @XmlElement(name="preferredWidth")
+    public int preferredWidth = 300;
+
+    /** The user's preferred height of the poll window. */
+    @XmlElement(name="preferredHeight")
+    public int preferredHeight = 300;
 
     public PosterCellServerState() {
         super();
@@ -44,6 +71,36 @@ public class PosterCellServerState extends CellServerState {
     @Override
     public String getServerClassName() {
         return "org.jdesktop.wonderland.modules.poster.server.PosterCellMO";
+    }
+
+    @XmlTransient public int getPreferredWidth () {
+        return preferredWidth;
+    }
+
+    public void setPreferredWidth (int preferredWidth) {
+        this.preferredWidth = preferredWidth;
+    }
+
+    @XmlTransient public int getPreferredHeight () {
+        return preferredHeight;
+    }
+
+    public void setPreferredHeight (int preferredHeight) {
+        this.preferredHeight = preferredHeight;
+    }
+
+    /**
+     * Returns a string representation of this class.
+     *
+     * @return The server state information as a string.
+     */
+    @Override
+    public String toString() {
+        return super.toString() + " [PosterCellServerState]: " +
+	    "preferredWidth=" + preferredWidth + "," +
+	    "preferredHeight=" + preferredHeight + "," +
+	    "pixelScaleX=" + pixelScaleX + "," +
+	    "pixelScaleY=" + pixelScaleY;
     }
 
 }
