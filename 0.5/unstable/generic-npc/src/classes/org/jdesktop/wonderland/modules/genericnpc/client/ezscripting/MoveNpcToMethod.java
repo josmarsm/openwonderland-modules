0708c7b65f51130f7922a9b6735f468a9acf61b7
pympnotiq <pympnotiq@gmail.com>
2011-01-28 19:20:51 +0000

    Fixed bug where NPC always walked to origin when calling MoveNPC() from a script

--- 0.5/unstable/generic-npc/src/classes/org/jdesktop/wonderland/modules/genericnpc/client/ezscripting/MoveNpcToMethod.java
+++ 0.5/unstable/generic-npc/src/classes/org/jdesktop/wonderland/modules/genericnpc/client/ezscripting/MoveNpcToMethod.java
@@ -28,7 +28,7 @@ public class MoveNpcToMethod implements ScriptMethodSPI {
     private NpcCell cell;
     private AvatarImiJME renderer;
     float x, y, z;
-    private Semaphore lock;
+    private Semaphore lock;// = new Semaphore(0);
     public String getFunctionName() {
         return "MoveNPC";
     }
@@ -38,9 +38,14 @@ public class MoveNpcToMethod implements ScriptMethodSPI {
             cell = (NpcCell)os[0];
             renderer = (AvatarImiJME)cell.getCellRenderer(RendererType.RENDERER_JME);
         }
-        float x = ((Double)os[1]).floatValue();
-        float y = ((Double)os[2]).floatValue();
-        float z = ((Double)os[3]).floatValue();
+         x = ((Double)os[1]).floatValue();
+         y = ((Double)os[2]).floatValue();
+         z = ((Double)os[3]).floatValue();
+        System.out.println("X: " + x + "\n"
+                          +"Y: " + y + "\n"
+                          +"Z: " + z + "\n");
+        lock = new Semaphore(0);
+
     }
 
     public void run() {
@@ -48,15 +53,17 @@ public class MoveNpcToMethod implements ScriptMethodSPI {
 
     }
 
-
     protected void move(float x, float y, float z) {
+        //lock = new Semaphore(0);
         GameContext context = renderer.getAvatarCharacter().getContext();
         CharacterBehaviorManager helm = context.getBehaviorManager();
         helm.clearTasks();
         helm.setEnable(true);
         helm.addTaskToTop(new BlockingGoTo(new Vector3f(x, y, z), context));
         try {
-            lock.acquire();
+            //System.out.println("[EZScript Debug] Available lock permits before acquire: "+ lock.availablePermits());
+            lock.acquire(); //--
+            //System.out.println("[EZScript Debug] Available lock permits after acquire: " +lock.availablePermits());
         } catch(InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -66,14 +73,11 @@ public class MoveNpcToMethod implements ScriptMethodSPI {
     }
 
 
-    class BlockingGoTo extends GoTo {
-        private boolean bDone = false;
-
-        
+    class BlockingGoTo extends GoTo {                
 
         public BlockingGoTo(Vector3f goalPosition, GameContext context) {
             super(goalPosition, context);
-            lock = new Semaphore(0);
+            
         }
 
         public BlockingGoTo(Vector3f goalPosition, Vector3f directionAtGoal, GameContext context) {
@@ -83,8 +87,11 @@ public class MoveNpcToMethod implements ScriptMethodSPI {
         @Override
         public void update(float deltaTime) {
             super.update(deltaTime);
-            if(bDone) {
-                lock.release();
+            if(!verify()) {
+                //System.out.println("[EZScript Debug] Available lock permits before release: "+lock.availablePermits());
+                lock.release(); //++
+                //System.out.println("[EZScript Debug] Available lock permits after release: "+lock.availablePermits());
+                //System.out.println("[EZScript] releasing lock in BlockingGoTo");
             }
         }
     }
