e69a66a02dd52849c7c2fd15d49607e70c8babbe
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2011-01-29 20:11:47 +0000

    Added a script method to show a message on the HUD, Added a script method to play a sound file from a URL

---
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/SPI/ShowHUDMessageMethod.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jdesktop.wonderland.modules.ezscript.client.SPI;
+
+import javax.swing.SwingUtilities;
+import org.jdesktop.wonderland.client.hud.CompassLayout.Layout;
+import org.jdesktop.wonderland.client.hud.HUD;
+import org.jdesktop.wonderland.client.hud.HUDComponent;
+import org.jdesktop.wonderland.client.hud.HUDManagerFactory;
+import org.jdesktop.wonderland.modules.ezscript.client.annotation.ReturnableScriptMethod;
+
+/**
+ *
+ * @author JagWire
+ */
+@ReturnableScriptMethod
+public class ShowHUDMessageMethod implements ReturnableScriptMethodSPI {
+
+    HUD hud;
+    HUDComponent component;
+    HUDMessagePanel panel;
+    String message;
+    public String getFunctionName() {
+        return "ShowHUDMessage";
+    }
+
+    public void setArguments(Object[] args) {
+        message = (String)args[0];
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                hud = HUDManagerFactory.getHUDManager().getHUD("main");
+                panel = new HUDMessagePanel(message);
+                component = hud.createComponent(panel);
+                panel.setComponent(component);
+                component.setDecoratable(true);
+                component.setPreferredLocation(Layout.CENTER);
+
+                hud.addComponent(component);
+            }
+        });
+
+    }
+
+    public void run() {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                component.setVisible(true);
+            }
+        });
+        //component.setVisible(true);
+    }
+
+    public Object returns() {
+        return component;
+    }
+}
