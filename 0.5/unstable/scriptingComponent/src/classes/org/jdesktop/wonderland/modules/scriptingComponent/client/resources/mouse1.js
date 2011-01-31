0fb3f250092bf9754dfd0514df01878e7ac6ecd0
morrishford <morrishford@df51345c-241b-11df-8743-957545a88a67>
2011-01-31 20:00:52 +0000

    Update for NPC changes and default scripts

--- 0.5/unstable/scriptingComponent/src/classes/org/jdesktop/wonderland/modules/scriptingComponent/client/resources/mouse1.js
+++ 0.5/unstable/scriptingComponent/src/classes/org/jdesktop/wonderland/modules/scriptingComponent/client/resources/mouse1.js
@@ -1,3 +1,3 @@
-// mouse1.js from component
-print("This a mouse1.js script from the scripting component resource area\r\n");
+// Component based default mouse1.js script
+print("This is the default script that executes if there is no mouse1.js script found otherwise\r\n");
 
