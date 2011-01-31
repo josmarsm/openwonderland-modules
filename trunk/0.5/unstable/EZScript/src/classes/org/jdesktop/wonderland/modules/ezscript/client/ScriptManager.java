b72d73611065acbea2637dce314b094bcf7948f4
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2011-01-31 22:32:13 +0000

    Making the script editors a bit more uniform.

--- trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/ScriptManager.java
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/ScriptManager.java
@@ -5,8 +5,20 @@
 
 package org.jdesktop.wonderland.modules.ezscript.client;
 
+import java.util.Iterator;
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.JDialog;
-import org.jdesktop.wonderland.client.input.InputManager;
+import javax.swing.SwingUtilities;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
+import org.jdesktop.wonderland.client.login.LoginManager;
+import org.jdesktop.wonderland.common.utils.ScannedClassLoader;
+import org.jdesktop.wonderland.modules.ezscript.client.SPI.ReturnableScriptMethodSPI;
+import org.jdesktop.wonderland.modules.ezscript.client.SPI.ScriptMethodSPI;
+import org.jdesktop.wonderland.modules.ezscript.client.annotation.ReturnableScriptMethod;
+import org.jdesktop.wonderland.modules.ezscript.client.annotation.ScriptMethod;
 
 /**
  *
@@ -16,7 +28,10 @@ public class ScriptManager {
 
     private ScriptEditorPanel scriptEditor;
     private JDialog dialog;
-    
+    private ScriptEditorPanel panel;
+    private ScriptEngineManager engineManager = new ScriptEngineManager(LoginManager.getPrimary().getClassloader());
+    private ScriptEngine scriptEngine = null;
+    private Bindings scriptBindings = null;
 
     private static ScriptManager instance;
 
@@ -29,19 +44,96 @@ public class ScriptManager {
 
     private ScriptManager() {
         dialog = new JDialog();
-
+        panel = new ScriptEditorPanel(dialog);
         dialog.setTitle("Script Editor - Wonderland Client");
         //2. Optional: What happens when the frame closes?
         dialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
 
         //3. Create component and put them in the frame.
-        dialog.setContentPane(new ScriptEditorPanel( dialog));
+        dialog.setContentPane(panel);
 
         //4. Size the frame.
         dialog.pack();
-        
+
+        //Next, acquire the scripting magicry
+        scriptEngine = engineManager.getEngineByName("JavaScript");
+        scriptBindings = scriptEngine.createBindings();
+
+        //Add the necessary script bindings
+        scriptBindings.put("Client", ClientContextJME.getClientMain());
+
+        //Load the methods into the library
+        ScannedClassLoader loader = LoginManager.getPrimary().getClassloader();
+        Iterator<ScriptMethodSPI> iter = loader.getInstances(ScriptMethod.class,
+                                                        ScriptMethodSPI.class);
+        //grab all global void methods
+        while(iter.hasNext()) {
+            final ScriptMethodSPI method = iter.next();
+            addFunctionBinding(method);
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    panel.addLibraryEntry(method.getFunctionName(),
+                                  method.getDescription());
+                }
+            });
+        }
+
+        //grab all returnablesa
+        Iterator<ReturnableScriptMethodSPI> returnables
+                            = loader.getInstances(ReturnableScriptMethod.class,
+                                               ReturnableScriptMethodSPI.class);
+        while(returnables.hasNext()) {
+            ReturnableScriptMethodSPI method = returnables.next();
+            addFunctionBinding(method);
+            panel.addLibraryEntry(method.getFunctionName(),
+                                  method.getDescription());
+        }
     }
+
     public void showScriptEditor() {
         dialog.setVisible(true);
+    }
+
+    public void addFunctionBinding(ScriptMethodSPI method) {
+        scriptBindings.put("this"+method.getFunctionName(), method);
+        String scriptx  = "function " + method.getFunctionName()+"() {\n"
+            + "\tvar args = java.lang.reflect.Array.newInstance(java.lang.Object, arguments.length);\n"
+            + "\tfor(var i = 0; i < arguments.length; i++) {\n"
+            + "\targs[i] = arguments[i];\n"
+            + "\t}\n"
+
+           // + "\targs = "+method.getFunctionName()+".arguments;\n"
+            + "\tthis"+method.getFunctionName()+".setArguments(args);\n"
+            + "\tthis"+method.getFunctionName()+".run();\n"
+            +"}";
+
+        try {
+            //System.out.println("evaluating script: \n"+scriptx);
+            scriptEngine.eval(scriptx, scriptBindings);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addFunctionBinding(ReturnableScriptMethodSPI method) {
+        scriptBindings.put("this"+method.getFunctionName(), method);
+        String scriptx  = "function " + method.getFunctionName()+"() {\n"
+            + "\tvar args = java.lang.reflect.Array.newInstance(java.lang.Object, arguments.length);\n"
+            + "\tfor(var i = 0; i < arguments.length; i++) {\n"
+            + "\t\targs[i] = arguments[i];\n"
+            + "\t}\n"
+            + "\tthis"+method.getFunctionName()+".setArguments(args);\n"
+            + "\tthis"+method.getFunctionName()+".run();\n"
+
+            + "\tvar tmp = this"+method.getFunctionName()+".returns();\n"
+            + "\treturn tmp\n;"
+            +"}";
+
+        try {
+           // System.out.println("evaluating script: \n"+scriptx);
+            scriptEngine.eval(scriptx, scriptBindings);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
     }
 }
