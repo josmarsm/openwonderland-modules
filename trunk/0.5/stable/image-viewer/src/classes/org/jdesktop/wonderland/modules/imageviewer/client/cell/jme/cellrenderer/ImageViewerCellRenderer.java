b2c7e46485699920baae72762eae54eeeb9ed5e9
jonathankap <jonathankap@df51345c-241b-11df-8743-957545a88a67>
2011-02-02 17:21:39 +0000

    Improve model bounds for images

--- trunk/0.5/stable/image-viewer/src/classes/org/jdesktop/wonderland/modules/imageviewer/client/cell/jme/cellrenderer/ImageViewerCellRenderer.java
+++ trunk/0.5/stable/image-viewer/src/classes/org/jdesktop/wonderland/modules/imageviewer/client/cell/jme/cellrenderer/ImageViewerCellRenderer.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2011, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -17,7 +35,7 @@
  */
 package org.jdesktop.wonderland.modules.imageviewer.client.cell.jme.cellrenderer;
 
-import com.jme.bounding.BoundingSphere;
+import com.jme.bounding.BoundingBox;
 import com.jme.image.Image;
 import com.jme.image.Texture;
 import com.jme.math.Vector3f;
@@ -105,7 +123,7 @@ public class ImageViewerCellRenderer extends BasicRenderer {
         // Create a box of suitable dimensions
         Box box = new Box("Box", new Vector3f(0, 0, 0), width, height, IMAGE_DEPTH);
         node.attachChild(box);
-        node.setModelBound(new BoundingSphere());
+        node.setModelBound(new BoundingBox(new Vector3f(0, 0, 0), width, height, IMAGE_DEPTH));
         node.updateModelBound();
 
         // Set the texture on the node
