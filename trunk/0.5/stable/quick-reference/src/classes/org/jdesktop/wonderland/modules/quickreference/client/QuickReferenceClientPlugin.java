e69440a32b1f5cee878899d4061950c21d37d4fa
jonathankap <jonathankap@df51345c-241b-11df-8743-957545a88a67>
2011-03-21 16:49:54 +0000

    Window menu properly reflect HUD window state  - fix for OWL issue #174  - patch contributed by Horst Rutter

--- trunk/0.5/stable/quick-reference/src/classes/org/jdesktop/wonderland/modules/quickreference/client/QuickReferenceClientPlugin.java
+++ trunk/0.5/stable/quick-reference/src/classes/org/jdesktop/wonderland/modules/quickreference/client/QuickReferenceClientPlugin.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2011, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -75,6 +93,7 @@ public class QuickReferenceClientPlugin extends BaseClientPlugin {
                     if (hudComponent == null) {
                         hudComponent = createHUDComponent();
                     }
+                    hudComponent.setMaximized();
                     hudComponent.setVisible(true);
                 }
                 else {
@@ -128,9 +147,14 @@ public class QuickReferenceClientPlugin extends BaseClientPlugin {
         // of the check box menu item too. We also need to turn off the camera.
         hudComponent.addEventListener(new HUDEventListener() {
             public void HUDObjectChanged(HUDEvent event) {
-                if (event.getEventType() == HUDEventType.CLOSED) {
-                    menuItem.setSelected(false);
-                }
+            	HUDEventType hudEventType = event.getEventType();
+                if (hudEventType == HUDEventType.CLOSED
+                		|| hudEventType == HUDEventType.MINIMIZED) {
+                	menuItem.setSelected(false);
+                } else 
+                if (hudEventType == HUDEventType.MAXIMIZED) {
+                	menuItem.setSelected(true);
+                } 
             }
         });
         
