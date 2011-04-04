af4d0cea072067df1b48954e245dd73a08e8b8e9
jonathankap <jonathankap@df51345c-241b-11df-8743-957545a88a67>
2011-04-04 18:19:03 +0000

    Separate out functions into utility class

--- 0.5/unstable/best-view/src/classes/org/jdesktop/wonderland/modules/bestview/client/BestViewComponent.java
+++ 0.5/unstable/best-view/src/classes/org/jdesktop/wonderland/modules/bestview/client/BestViewComponent.java
@@ -17,26 +17,15 @@
  */
 package org.jdesktop.wonderland.modules.bestview.client;
 
-import com.jme.bounding.BoundingBox;
-import com.jme.bounding.BoundingSphere;
-import com.jme.bounding.BoundingVolume;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 import com.jme.scene.CameraNode;
-import com.jme.scene.Geometry;
-import com.jme.scene.Node;
-import com.jme.scene.Spatial;
-import java.awt.Canvas;
 import java.awt.event.MouseWheelEvent;
-import java.util.Collection;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
-import org.jdesktop.mtgame.Entity;
-import org.jdesktop.mtgame.RenderComponent;
 import org.jdesktop.wonderland.client.cell.Cell;
 import org.jdesktop.wonderland.client.cell.CellComponent;
 import org.jdesktop.wonderland.client.cell.annotation.UsesCellComponent;
-import org.jdesktop.wonderland.client.cell.utils.CellPlacementUtils;
 import org.jdesktop.wonderland.client.contextmenu.ContextMenuActionListener;
 import org.jdesktop.wonderland.client.contextmenu.ContextMenuItem;
 import org.jdesktop.wonderland.client.contextmenu.ContextMenuItemEvent;
@@ -47,9 +36,7 @@ import org.jdesktop.wonderland.client.input.Event;
 import org.jdesktop.wonderland.client.input.EventClassFocusListener;
 import org.jdesktop.wonderland.client.jme.CameraController;
 import org.jdesktop.wonderland.client.jme.ClientContextJME;
-import org.jdesktop.wonderland.client.jme.JmeClientMain;
 import org.jdesktop.wonderland.client.jme.ViewManager;
-import org.jdesktop.wonderland.client.jme.cellrenderer.CellRendererJME;
 import org.jdesktop.wonderland.client.jme.input.MouseEvent3D;
 import org.jdesktop.wonderland.client.jme.input.MouseWheelEvent3D;
 import org.jdesktop.wonderland.client.scenemanager.event.ContextEvent;
@@ -122,7 +109,7 @@ public class BestViewComponent extends CellComponent
      * Called when the context menu is clicked
      */
     public void actionPerformed(ContextMenuItemEvent event) {
-        CellTransform xform = getBestView();
+        CellTransform xform = BestViewUtils.getBestView(cell);
 
         CameraController prev = ViewManager.getViewManager().getCameraController();
         CellTransform viewLocation = ViewManager.getViewManager().getPrimaryViewCell().getWorldTransform();
@@ -133,171 +120,7 @@ public class BestViewComponent extends CellComponent
         ClientContextJME.getViewManager().setCameraController(camera);
     }
 
-    /**
-     * Find the best view of a cell
-     * @return the position of a cell's best view
-     */
-    protected CellTransform getBestView() {
-        ViewManager vm = ViewManager.getViewManager();
-        BoundingVolume bounds = getModelBounds();
-
-        // find out how far we should be away from the object
-        float fov = vm.getViewProperties().getFieldOfView();
-
-        // calculate the fov in the X dimension, since it might not be the same
-        // if the screen isn't square
-        Canvas c = JmeClientMain.getFrame().getCanvas();
-        int width = c.getWidth();
-        int height = c.getHeight();
-
-        // fov is calculated in the y dimension, so calculate the x and
-        // y fovs based on the y value
-        float fovX = fov;
-        float fovY = fov;
-
-        float ratio = (float) width / (float) height;
-        fovX *= ratio;
-        
-        // calculate bounds, x and y distances
-        float xRadius = 1.0f;
-        float yRadius = 1.0f;
-        float z = 1.0f;
-        if (bounds instanceof BoundingSphere) {
-            xRadius = yRadius = z = ((BoundingSphere)bounds).radius;
-        } else if (bounds instanceof BoundingBox) {
-            BoundingBox bb = (BoundingBox) bounds;
-            xRadius = bb.xExtent;
-            yRadius = bb.yExtent;
-            z = bb.zExtent;
-        }
-
-        // find the X and Y distances
-        float distance = Math.max(getDistance(xRadius, z, fovX),
-                                  getDistance(yRadius, z, fovY));
-
-        // add a small fudge factor so we can see borders
-        distance += 0.2f;
-
-        LOGGER.fine("Calculated distance to " + bounds + " is: " +
-                    " x: " + getDistance(xRadius, z, fovX) + " for fov " + fovX +
-                    " y: " + getDistance(yRadius, z, fovY) + " for fov " + fovY);
-
-        // calculate the look vector to this cell -- we only care about the y axis
-        // rotation
-        Quaternion rotation = cell.getWorldTransform().getRotation(null);
-        Vector3f lookVec = CellPlacementUtils.getLookDirection(rotation, null);
-
-        LOGGER.fine("Look vector: " + lookVec);
-
-        // translate into a quaternion using lookAt
-        Quaternion look = new Quaternion();
-        look.lookAt(lookVec.negate(), Vector3f.UNIT_Y);
-
-        // find the origin by translating the look vector
-        Vector3f origin = lookVec.mult(distance);
-
-        LOGGER.fine("Look vector x distance = " + origin);
-
-        origin.addLocal(cell.getWorldTransform().getTranslation(null));
-
-        LOGGER.fine("Origin + " + cell.getWorldTransform().getTranslation(null) +
-                    " = " + origin);
-
-        return new CellTransform(look, origin);
-    }
-
-    /**
-     * Find the bounds of the model associated with this component's cell. If
-     * minimal bounds cannot be found, use the cell's world bounds (which
-     * are not likely to be accurate).
-     *
-     * @return the best possible bounds we can find for this object
-     */
-    private BoundingVolume getModelBounds() {
-        // find a JME renderer for the cell
-        CellRendererJME renderer = (CellRendererJME)
-                cell.getCellRenderer(Cell.RendererType.RENDERER_JME);
-        if (renderer == null) {
-            return cell.getWorldBounds();
-        }
-
-        // next get the root of the scene graph
-        Entity e = renderer.getEntity();
-        RenderComponent rc = e.getComponent(RenderComponent.class);
-        if (rc == null || rc.getSceneRoot() == null) {
-            return cell.getWorldBounds();
-        }
-
-        // calculate the bounds from the root of the scene graph
-        return getModelBounds(rc.getSceneRoot());
-    }
-
-    /**
-     * Find the bounds of a node. This is different than node.getWorldBounds()
-     * because we try to calculate the minimal bounds for the object without
-     * taking rotation into account. For flat objects (like 2D windows), this
-     * makes a significant difference in the calculated size.
-     *
-     * @param node the node to calculate the bounds of
-     * @return the calculated bounds
-     */
-    private BoundingVolume getModelBounds(Node node) {
-        BoundingVolume out = null;
-
-        Collection<Spatial> children = node.getChildren();
-        if (children == null) {
-            return out;
-        }
-
-        // caclulate the bounds by merging the bounds of all children
-        for (Spatial s : children) {
-            BoundingVolume childBounds = null;
-            if (s instanceof Geometry) {
-                childBounds = getModelBounds((Geometry) s);
-            } else if (s instanceof Node) {
-                childBounds = getModelBounds((Node) s);
-            }
-
-            if (out == null) {
-                out = childBounds;
-            } else {
-                out.mergeLocal(childBounds);
-            }
-        }
-
-        return out;
-    }
-
-    /**
-     * Get the model bounds of a piece of geometry. This returns the properly
-     * scaled bounds, but with no rotation or offset.
-     * @param g the object to caculate the bounds of
-     * @return the scaled bounds
-     */
-    private BoundingVolume getModelBounds(Geometry g) {
-        BoundingVolume clone = g.getModelBound().clone(null);
-        clone = clone.transform(new Quaternion(), Vector3f.ZERO,
-                                g.getWorldScale(), clone);
-        return clone;
-    }
-
-    /**
-     * Find the optimal distance for the given radius and field-of-view
-     *
-     * @param radius the radius to fit in the field of view
-     * @param z the z offset to add to the result
-     * @param fov the field of view to calculate for
-     */
-    private static float getDistance(float radius, float z, float fov) {
-        double eyeDist = radius / Math.tan(Math.toRadians(fov / 2.0));
-
-        // To make sure the front face of the Cell is in-view we must add the
-        // bounds of the Cell along the z-axis
-        eyeDist += z;
-
-        return (float) eyeDist;
-    }
-
+    
     /**
      * A camera controller that puts the camera at the best view position,
      * and allows zooming in or out from there.
