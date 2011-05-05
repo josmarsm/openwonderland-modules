8c35db2dca9884725edb9275d912599029877d20
pympnotiq <pympnotiq@gmail.com>
2011-05-05 17:47:40 +0000

    Who is ryan? Changed authorship. ;)

--- 0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/StrafeRightMethod.java
+++ 0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/StrafeRightMethod.java
@@ -16,7 +16,7 @@ import org.jdesktop.wonderland.modules.ezscript.client.annotation.ScriptMethod;
 
 /**
  *
- * @author ryan
+ * @author JagWire
  */
 @ScriptMethod
 public class StrafeRightMethod implements ScriptMethodSPI {
