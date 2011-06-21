9f2f07e8a55b7582d389da8f4f39b332f4f5f393
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2011-06-21 15:02:09 +0000

    Added CloseHUDWindow command.

--- trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/WaitMethod.java
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/WaitMethod.java
@@ -5,20 +5,27 @@
 
 package org.jdesktop.wonderland.modules.ezscript.client.methods;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import org.jdesktop.wonderland.modules.ezscript.client.SPI.ScriptMethodSPI;
 import org.jdesktop.wonderland.modules.ezscript.client.annotation.ScriptMethod;
 
 /**
  *
  * @author JagWire
  */
-//@ScriptMethod
-public class WaitMethod extends ProcessorBasedMethod {
+@ScriptMethod
+public class WaitMethod implements ScriptMethodSPI {
 
     int seconds = 0;
     int fps = 30;
     int fpsCounter = 0;
+    private Semaphore lock = new Semaphore(0);
 
     public String getFunctionName() {
         return "Wait";
@@ -26,7 +33,7 @@ public class WaitMethod extends ProcessorBasedMethod {
 
     @Override
     public void setArguments(Object[] args) {
-        super.setArguments(args);
+        //super.setArguments(args);
        seconds = ((Double)args[1]).intValue();
     }
 
@@ -36,31 +43,30 @@ public class WaitMethod extends ProcessorBasedMethod {
     }
 
 
-    @Override
-    public void _compute() {
-        fpsCounter += 1;
-        //throw new UnsupportedOperationException("Not supported yet.");
-
-    }
-
-    @Override
-    public void _commit() {
-        //do nothing
-    }
-
-    @Override
-    public boolean finished() {
-        if(fpsCounter >= fps*seconds)
-            return true;
-
-        return false;
-    }
-
-    public Object returns() {
-        return "";
-    }
-
     public String getCategory() {
-        return "animation";
+        return "utilities";
+    }
+
+    public void run() {
+        SwingUtilities.invokeLater(new Runnable() { 
+            public void run() {
+               Timer timer = new Timer(seconds * 1000, new ActionListener() {
+                    public void actionPerformed(ActionEvent ae) {
+                        lock.release();
+                    }
+       
+                });
+                timer.setRepeats(false);
+                
+                timer.start();
+            }
+        });
+
+        try {
+            lock.acquire();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(WaitMethod.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
     }
 }
