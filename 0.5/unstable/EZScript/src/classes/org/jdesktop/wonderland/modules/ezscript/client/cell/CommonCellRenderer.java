319b76ad38f5ca4cb93bd142389b9bb721472ac2
pympnotiq <pympnotiq@gmail.com>
2011-07-11 17:22:39 +0000

    fix for CommonCellRenderer not rendering models added dynamically.

--- 0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/cell/CommonCellRenderer.java
+++ 0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/cell/CommonCellRenderer.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.jdesktop.mtgame.Entity;
 import org.jdesktop.mtgame.processor.WorkProcessor.WorkCommit;
 import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
 import org.jdesktop.wonderland.client.jme.SceneWorker;
 import org.jdesktop.wonderland.client.jme.artimport.DeployedModel;
 import org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer;
@@ -28,10 +29,10 @@ public class CommonCellRenderer extends BasicRenderer {
         modelsRoot = new Node();
 
         if(models.isEmpty())
-            return new Node();
+            return modelsRoot;
 
         for(DeployedModel model: models.values()) {
-            modelsRoot.attachChild(model.getModelLoader().loadDeployedModel(model, null));
+            modelsRoot.attachChild(model.getModelLoader().loadDeployedModel(model, entity));
         }
         
         return modelsRoot;
@@ -46,7 +47,8 @@ public class CommonCellRenderer extends BasicRenderer {
     public void update() {
         SceneWorker.addWorker(new WorkCommit() {
             public void commit() {
-                modelsRoot = new Node();
+                modelsRoot.detachAllChildren();
+                //modelsRoot = new Node();
 
                 if(models.isEmpty()) {
                     //finish up here
@@ -55,15 +57,19 @@ public class CommonCellRenderer extends BasicRenderer {
                 }
 
                 for(DeployedModel model: models.values()) {
-                    Node modelNode = model.getModelLoader().loadDeployedModel(model, null);
+                    logger.warning("Processing model: "+model.getModelURL());
+                    Node modelNode = model.getModelLoader().loadDeployedModel(model, entity);
                     modelNode.updateModelBound();
                     modelNode.updateGeometricState(0, true);
+                    modelNode.setVisible(true);
+                    modelNode.setIsCollidable(true);
                     
                     modelsRoot.attachChild(modelNode);
                 }
 
                 modelsRoot.updateModelBound();
                 modelsRoot.updateGeometricState(0, true);
+                ClientContextJME.getWorldManager().addToUpdateList(modelsRoot);
                 //finish up here too
             }
         });
