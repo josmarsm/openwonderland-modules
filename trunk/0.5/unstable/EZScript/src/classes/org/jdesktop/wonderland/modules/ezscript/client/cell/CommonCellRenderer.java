ac6cf0f301d62185520855410b33a1cc3a50f4c5
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2011-08-18 22:07:07 +0000

    Common cells now persist their models across snapshots.

--- trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/cell/CommonCellRenderer.java
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/cell/CommonCellRenderer.java
@@ -2,15 +2,24 @@
 package org.jdesktop.wonderland.modules.ezscript.client.cell;
 
 import com.jme.scene.Node;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jdesktop.mtgame.Entity;
 import org.jdesktop.mtgame.processor.WorkProcessor.WorkCommit;
 import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.cell.asset.AssetUtils;
 import org.jdesktop.wonderland.client.jme.ClientContextJME;
 import org.jdesktop.wonderland.client.jme.SceneWorker;
 import org.jdesktop.wonderland.client.jme.artimport.DeployedModel;
+import org.jdesktop.wonderland.client.jme.artimport.LoaderManager;
 import org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer;
+import org.jdesktop.wonderland.modules.sharedstate.common.SharedData;
+import org.jdesktop.wonderland.modules.sharedstate.common.SharedString;
 
 /**
  *
@@ -73,5 +82,38 @@ public class CommonCellRenderer extends BasicRenderer {
                 //finish up here too
             }
         });
+    }
+    
+    
+    public void initializeModels() {
+        
+        //grab the persisted map of models from our cell
+        Map<String, SharedData> modelsMap = ((CommonCell)cell).getModels();
+        
+        //acquire the LoaderManager to load our model.        
+        LoaderManager manager = LoaderManager.getLoaderManager();
+        
+        //for every entry in the cell's shared map
+        for ( Map.Entry<String, SharedData> e : modelsMap.entrySet()) {
+            try {
+                //acquire the url in string form 
+                String urlString = ((SharedString)e.getValue()).getValue();
+                
+                //Get the URL of the model in webDAV. Typically something like: wla://restaurant-activity/ etc...
+                URL url = AssetUtils.getAssetURL(urlString);
+
+                //Create the deployed model object from our recently acquired URL.
+                DeployedModel model = manager.getLoaderFromDeployment(url);
+                
+                //put in our local map
+                models.put(e.getKey(), model);
+                
+            } catch (IOException ex) {
+                Logger.getLogger(CommonCellRenderer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        //finally, re-render the common cell with the appropriate models.
+        update();
     }
 }
