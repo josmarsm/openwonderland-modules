1a5e5ff952556e3e08784009b7a9f7547cc8f04b
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2011-09-22 21:15:26 +0000

    ShowHUDImageMethod validates URL's location before displaying it.

--- trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/ShowHUDImageMethod.java
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/ShowHUDImageMethod.java
@@ -5,6 +5,9 @@
 
 package org.jdesktop.wonderland.modules.ezscript.client.methods;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,9 +27,10 @@ import org.jdesktop.wonderland.modules.ezscript.client.annotation.ReturnableScri
 @ReturnableScriptMethod
 public class ShowHUDImageMethod implements ReturnableScriptMethodSPI {
     
-    private HUDComponent component;
+    private HUDComponent lastComponent;
     private String urlName;
     private boolean fail = false;
+    private static final Logger logger = Logger.getLogger(ShowHUDImageMethod.class.getName());
     public String getDescription() {
         return "Shows an image on the Head's Up Display.\n" +
                 "-- usage: ShowHUDImage(\"www.openwonderland.org/image.jpg\");\n" +
@@ -45,14 +49,14 @@ public class ShowHUDImageMethod implements ReturnableScriptMethodSPI {
     public void setArguments(Object[] args) {
         fail = false;
         urlName = (String)args[0];        
-        component = null;
+        //component = null;
     }
 
     public Object returns() {
         if(fail)
             return null;
 
-        return component;
+        return lastComponent;
     }
 
     public void run() {
@@ -61,23 +65,44 @@ public class ShowHUDImageMethod implements ReturnableScriptMethodSPI {
         }
 
         try {
+            int responseCode = getResponseCode(urlName);
+            logger.warning("Response code for URL: "+responseCode);
+            if(getResponseCode(urlName) != 200) {
+                logger.warning("Error with URL!");
+                return;
+            }
+            
             final URL url = new URL(urlName);
-
+            
             SwingUtilities.invokeLater(new Runnable() {
 
                 public void run() {
                     HUD hud = HUDManagerFactory.getHUDManager().getHUD("main");
+                    HUDComponent component = null;
                     ImageIcon i = new ImageIcon(url);
+
                     component = hud.createImageComponent(i);
                     component.setDecoratable(true);
                     component.setPreferredLocation(Layout.NORTH);
 
                     hud.addComponent(component);
                     component.setVisible(true);
+                    
+                    lastComponent = component;
                 }
             });
         } catch (Exception ex) {
             Logger.getLogger(ShowHUDImageMethod.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    static int getResponseCode(String urlString) throws MalformedURLException, IOException {
+        URL u = new URL(urlString);
+        HttpURLConnection huc = (HttpURLConnection) u.openConnection();
+        huc.setRequestMethod("GET");
+        huc.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+
+        huc.connect();
+        return huc.getResponseCode();
     }
 }
