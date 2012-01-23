b2fa85a4423a80c61f3a9995a314ed2be8b4e251
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2012-01-23 20:52:39 +0000

    ShowHUDMessage now accepts an optional font-size argument.

--- trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/ShowHUDMessageMethod.java
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/methods/ShowHUDMessageMethod.java
@@ -5,6 +5,7 @@
 
 package org.jdesktop.wonderland.modules.ezscript.client.methods;
 
+import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 import org.jdesktop.wonderland.client.hud.CompassLayout.Layout;
 import org.jdesktop.wonderland.client.hud.HUD;
@@ -20,29 +21,56 @@ import org.jdesktop.wonderland.modules.ezscript.client.annotation.ReturnableScri
 @ReturnableScriptMethod
 public class ShowHUDMessageMethod implements ReturnableScriptMethodSPI {
 
-    HUD hud;
-    HUDComponent component;
-    HUDMessagePanel panel;
-    String message;
+    private HUD hud;
+    private HUDComponent component;
+    private HUDMessagePanel panel;
+    private String message;
+    private static final Logger logger = 
+                    Logger.getLogger(ShowHUDMessageMethod.class.getName());
+    
+    /** optional */
+    private float fontSize;
+    
     public String getFunctionName() {
         return "ShowHUDMessage";
     }
 
     public void setArguments(Object[] args) {
-        message = (String)args[0];
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                hud = HUDManagerFactory.getHUDManager().getHUD("main");
-                panel = new HUDMessagePanel(message);
-                component = hud.createComponent(panel);
-                panel.setComponent(component);
-                component.setDecoratable(true);
-                component.setPreferredLocation(Layout.NORTH);
-                
-                hud.addComponent(component);
-            }
-        });
+        if (args.length == 2) {
+            message = (String) args[0];
+            fontSize = ((Double) args[1]).floatValue();
 
+            logger.warning("Casting: "+args[1]+" to: "+fontSize);
+            SwingUtilities.invokeLater(new Runnable() {
+
+                public void run() {
+                    hud = HUDManagerFactory.getHUDManager().getHUD("main");
+                    panel = new HUDMessagePanel(message, fontSize);
+                    component = hud.createComponent(panel);
+                    panel.setComponent(component);
+                    component.setDecoratable(true);
+                    component.setPreferredLocation(Layout.NORTH);
+
+                    hud.addComponent(component);
+                }
+            });
+        } else {
+            message = (String)args[0];
+            SwingUtilities.invokeLater(new Runnable() {
+
+                public void run() {
+                    hud = HUDManagerFactory.getHUDManager().getHUD("main");
+                    panel = new HUDMessagePanel(message);
+                    component = hud.createComponent(panel);
+                    panel.setComponent(component);
+                    component.setDecoratable(true);
+                    component.setPreferredLocation(Layout.NORTH);
+
+                    hud.addComponent(component);
+                }
+            });
+            
+        }        
     }
 
     public void run() {
@@ -58,7 +86,8 @@ public class ShowHUDMessageMethod implements ReturnableScriptMethodSPI {
     }
 
     public String getDescription() {
-        return "usage: var c = ShowHUDMessage(string message)\n\n"
+        return "usage: var c = ShowHUDMessage(message)\n"
+                + "usage: var c = ShowHUDMessage(message, fontSize)\n\n"
                 +"-shows a window on the HUD with the specified string.\n"
                 +"-GREAT for simulating conversations onApproach with NPCs\n"
                 +"-returns a hud component object.";
