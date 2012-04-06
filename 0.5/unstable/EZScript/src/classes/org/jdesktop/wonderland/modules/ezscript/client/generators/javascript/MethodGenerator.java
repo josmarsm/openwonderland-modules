b3595cbd64274124aeecd6c64027be8520a32fa5
pympnotiq <pympnotiq@gmail.com>
2012-04-06 16:37:05 +0000

    Modularized script command generation. Integrated into component.

---
+++ 0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/generators/javascript/MethodGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jdesktop.wonderland.modules.ezscript.client.generators.javascript;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import org.jdesktop.wonderland.modules.ezscript.client.SPI.ScriptMethodSPI;
+import org.jdesktop.wonderland.modules.ezscript.client.generators.GeneratorSPI;
+
+/**
+ *
+ * @author Ryan
+ */
+public class MethodGenerator implements GeneratorSPI {
+    private final ScriptEngine engine;
+    private final Bindings bindings;
+    private ScriptMethodSPI method;
+
+    public MethodGenerator(ScriptEngine engine, Bindings bindings) {
+//        super(engine, bindings);
+        this.engine = engine;
+        this.bindings = bindings;
+    }
+    
+    public void setActiveMethod(ScriptMethodSPI method) {
+        this.method = method;
+    }
+    
+    @Override
+    public String generateScriptBinding() {
+       
+        bindings.put("this"+method.getFunctionName(), method);
+        String scriptx  = "function " + method.getFunctionName()+"() {\n"
+            + "\tvar args = java.lang.reflect.Array.newInstance(java.lang.Object, arguments.length);\n"
+            + "\tfor(var i = 0; i < arguments.length; i++) {\n"
+            + "\targs[i] = arguments[i];\n"
+            + "\t}\n"
+
+           // + "\targs = "+method.getFunctionName()+".arguments;\n"
+            + "\tthis"+method.getFunctionName()+".setArguments(args);\n"
+            + "\tthis"+method.getFunctionName()+".run();\n"
+            +"}";
+        
+        return scriptx;
+    }
+}
---
+++ 0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/generators/javascript/ReturnableMethodGenerator.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jdesktop.wonderland.modules.ezscript.client.generators.javascript;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import org.jdesktop.wonderland.modules.ezscript.client.SPI.ReturnableScriptMethodSPI;
+import org.jdesktop.wonderland.modules.ezscript.client.generators.GeneratorSPI;
+
+/**
+ *
+ * @author JagWire
+ */
+public class ReturnableMethodGenerator implements GeneratorSPI {
+    private ReturnableScriptMethodSPI method;
+    private final Bindings bindings;
+    private final ScriptEngine scriptEngine;
+
+    public ReturnableMethodGenerator(ScriptEngine scriptEngine, Bindings bindings) {
+        this.scriptEngine = scriptEngine;
+        this.bindings = bindings;
+    }
+    
+    public void setActiveMethod(ReturnableScriptMethodSPI method) {
+        this.method = method;
+    }
+    
+        
+    public String generateScriptBinding() {
+        bindings.put("this"+method.getFunctionName(), method);
+        
+        String scriptx  = "function " + method.getFunctionName()+"() {\n"
+            + "\tvar args = java.lang.reflect.Array.newInstance(java.lang.Object, arguments.length);\n"
+            + "\tfor(var i = 0; i < arguments.length; i++) {\n"
+            + "\t\targs[i] = arguments[i];\n"
+            + "\t}\n"
+            + "\tthis"+method.getFunctionName()+".setArguments(args);\n"
+            + "\tthis"+method.getFunctionName()+".run();\n"
+
+            + "\tvar tmp = this"+method.getFunctionName()+".returns();\n"
+            + "\treturn tmp\n;"
+            +"}";
+
+        return scriptx;
+    }
+    
+}
