49f86e0cb6f768c0e827808955ffd2d3d449dde4
pympnotiq@gmail.com <pympnotiq@gmail.com@df51345c-241b-11df-8743-957545a88a67>
2012-04-09 17:14:46 +0000

    added support for multiple argument methods in EventBridges. Added support for scripting camera updates via CameraController and CameraContext bindings.

---
+++ trunk/0.5/unstable/EZScript/src/classes/org/jdesktop/wonderland/modules/ezscript/client/SPI/EventObjectSPI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jdesktop.wonderland.modules.ezscript.client.SPI;
+
+/**
+ *
+ * @author JagWire
+ */
+public interface EventObjectSPI {
+    public String getEventName();
+    public int getNumberOfArguments();
+}
